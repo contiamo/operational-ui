ed3aabc63b276fa2fa718948c8c9c14b7b02b427
Tejas Kumar <tejas@contiamo.com>
2017-08-04 15:33:28 +0200

    Removed some clutter

--- src/Navigation/Navigation.style.js
+++
@@ -1,8 +0,0 @@
-// @flow
-export default (props: void, theme: THEME): {} => ({
-  width: 250,
-  height: '100vh',
-  overflow: 'auto',
-  backgroundColor: theme.greys[20],
-  color: theme.greys.white,
-});
