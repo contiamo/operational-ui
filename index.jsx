8c9b700c0888cd1ce5b4fab7e76c380d51905cc7
Tejas Kumar <tejas@contiamo.com>
2017-08-04 16:26:32 +0200

    Implemented Rollup: we can now export things

--- index.jsx
+++ index.jsx
@@ -1,25 +1,7 @@
-import 'react-hot-loader/patch';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import glamorous, { ThemeProvider } from 'glamorous';
-import { AppContainer } from 'react-hot-loader';
+import SideNavigation, {
+  SideNavigationItem,
+  SideNavigationLink,
+  SideNavigationTooltip,
+} from './src/components/SideNavigation/SideNavigation';
 
-import App from './src/App';
-
-const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <App />
-    </AppContainer>,
-    document.querySelector('#app'),
-  );
-};
-
-render(App);
-
-if (module.hot) {
-  module.hot.accept('./src/App', () => {
-    const NextAppContainer = require('./src/App');
-    render(NextAppContainer);
-  });
-}
+export { SideNavigation, SideNavigationItem, SideNavigationLink, SideNavigationTooltip };
---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import image from 'rollup-plugin-image';
+import babel from 'rollup-plugin-babel';
+import jsx from 'rollup-plugin-jsx';
+import flow from 'rollup-plugin-flow';
+
+export default {
+  entry: './index.jsx',
+  format: 'es',
+  plugins: [
+    resolve({ extensions: ['.js', '.jsx'], jsnext: true, main: true }),
+    commonjs({
+      esnext: true,
+      main: true,
+    }),
+    image(),
+    babel({
+      plugins: ['external-helpers'],
+      exclude: 'node_modules/**', // only transpile our source code
+    }),
+    jsx({
+      factory: 'React.createElement',
+    }),
+    flow(),
+  ],
+  external: ['react', 'react-dom', 'react-router', 'react-router-dom', 'jest'],
+  dest: './dist/bundle.js', // equivalent to --output
+};
