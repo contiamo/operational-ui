4817625927ec85fa918dbcb92bfb951f20f91c36
Tejas Kumar <tejas@contiamo.com>
2017-08-14 13:09:13 +0200

    Added plus chip component

---
+++ src/components/PlusChip/PlusChip.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import glamorous from 'glamorous';
+
+import { hexOrColor } from '../../utils/color';
+
+const PlusChip = ({
+  className,
+  children,
+  onClick,
+}: {
+  className?: string,
+  children?: string,
+  onClick?: any,
+}) =>
+  (<div className={className} onClick={onClick}>
+    {children}
+  </div>);
+
+const style = ({ theme, color }: { theme: THEME, color: string }) => {
+  const borderColor = color
+    ? hexOrColor(color)((theme.colors && theme.colors[color]) || 'white')
+    : theme.colors.primary;
+
+  return {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 16,
+    height: 16,
+    border: '1px solid',
+    color: borderColor,
+
+    '& + &': {
+      marginLeft: theme.spacing && theme.spacing / 2,
+    },
+  };
+};
+
+export default glamorous(PlusChip)(style);
+export { PlusChip };
---
+++ src/components/PlusChip/__tests__/PlusChip.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { PlusChip } from '../PlusChip';
+
+describe('Plus Chip', () => {
+  it('Should correctly render', () => {
+    const renderedComponent = shallow(<PlusChip />);
+    expect(renderedComponent).toMatchSnapshot();
+  });
+  it('Should respond to click events', () => {
+    const fn = jest.fn();
+    const renderedComponent = shallow(<PlusChip onClick={fn} />);
+
+    renderedComponent.simulate('click');
+
+    expect(fn).toHaveBeenCalled();
+  });
+});
