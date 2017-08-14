0de8cfd21c030532209a23aa3584a6c3666f7a0c
Tejas Kumar <tejas@contiamo.com>
2017-08-14 12:51:53 +0200

    Added a Stat component

---
+++ src/components/Stat/__tests__/Stat.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Stat from '../Stat';
+
+describe('Stat', () => {
+  it('Should render', () => {
+    const renderedComponent = shallow(<Stat label="Country">DE</Stat>);
+    expect(renderedComponent).toMatchSnapshot();
+  });
+});
