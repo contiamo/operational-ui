5b1927072d5032d914591c4ece6ee1111ef34fd2
Tejas Kumar <tejas@contiamo.com>
2017-08-15 10:11:07 +0200

    Updated lib and export

--- index.jsx
+++ index.jsx
@@ -1,25 +1,35 @@
 // Simple imports n' exports for consumers of the library.
 
+import Header, { HeaderItem, HeaderSeparator, HeaderTitle } from './src/components/Header/Header';
+
 import SideNavigation, {
   SideNavigationItem,
   SideNavigationLink,
   SideNavigationTooltip,
 } from './src/components/SideNavigation/SideNavigation';
 
-import Header, { HeaderItem, HeaderSeparator, HeaderTitle } from './src/components/Header/Header';
+import { Chip } from './src/components/Chip/Chip';
+import { PlusChip } from './src/components/PlusChip/PlusChip';
+import { Card } from './src/components/Card/Card';
+import { Stat } from './src/components/Stat/Stat';
 
 import contiamoTheme from './src/App.theme';
+
 import { ThemeProvider } from 'glamorous';
 
 export {
-  SideNavigation,
-  SideNavigationItem,
-  SideNavigationLink,
-  SideNavigationTooltip,
   Header,
   HeaderItem,
   HeaderSeparator,
   HeaderTitle,
+  SideNavigation,
+  SideNavigationItem,
+  SideNavigationLink,
+  SideNavigationTooltip,
+  Chip,
+  PlusChip,
+  Card,
+  Stat,
   contiamoTheme,
   ThemeProvider,
 };
