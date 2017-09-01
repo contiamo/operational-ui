a7562011f1dd7d48d0b8ec2e35385bd87887e13c
Peter Szerzo <peterszerzo@Peters-MacBook-Pro.local>
2017-09-01 16:46:26 +0200

    Add raw-loader to import documentation snippets as strings

--- packages/showcase/src/pages/Buttons/buttons.snippet.jsx
+++ packages/showcase/src/pages/Buttons/buttons.snippet.jsx
@@ -1,9 +1,9 @@
-export default `
-<div style={{display: 'flex', marginTop: 16 }}>
+<div style={{ display: "flex", marginTop: 16 }}>
   <Button color="#5F8E2C">Button 1</Button>
   <Button>Button 2</Button>
   <Button modifiers={["space"]}>Group 1</Button>
-  <Button active modifiers={["group"]}>Group 2</Button>
+  <Button active modifiers={["group"]}>
+    Group 2
+  </Button>
   <Button modifiers={["group"]}>Group 3</Button>
 </div>
-`
