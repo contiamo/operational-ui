b94fa4fe4b13300812d58da1b79a8b3ff276a0b9
Peter Szerzo <peterszerzo@Peters-MacBook-Pro.local>
2017-09-04 11:30:23 +0200

    Button modifiers prototype

--- packages/showcase/src/pages/Buttons/snippet.js
+++ packages/showcase/src/pages/Buttons/snippet.js
@@ -1,9 +1,9 @@
 export default `
 <div style={{display: 'flex', marginTop: 16 }}>
   <Button color="#5F8E2C">Button 1</Button>
-  <Button className="Button_space">Button 2</Button>
-  <Button className="Button_space">Group 1</Button>
-  <Button active className="Button_group">Group 2</Button>
-  <Button className="Button_group">Group 3</Button>
+  <Button modifiers={["space"]}>Button 2</Button>
+  <Button modifiers={["space"]}>Group 1</Button>
+  <Button active modifiers={["group"]}>Group 2</Button>
+  <Button modifiers={["group"]}>Group 3</Button>
 </div>
 `
