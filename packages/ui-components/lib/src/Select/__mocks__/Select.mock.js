acd33ee5915bf3825c103ef8b97a39d8edacb96b
Tejas Kumar <tejas@contiamo.com>
2017-09-07 19:24:48 +0200

    Removed compiled things

--- packages/ui-components/lib/src/Select/__mocks__/Select.mock.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.options = [
-    { id: 1, label: "John", value: -10 },
-    { id: 2, label: "Joey", value: "Nein" },
-    { id: 3, label: "Tupac", value: true },
-    { id: 4, label: "Chandler", value: [{ alive: true }] }
-];
-//# sourceMappingURL=Select.mock.js.map
