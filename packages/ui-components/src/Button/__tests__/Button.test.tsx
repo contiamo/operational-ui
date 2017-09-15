dd250559d8f55402318d239feaa1d15ec1ceb4e6
Peter Szerzo <peterszerzo@Peters-MacBook-Pro.local>
2017-09-15 10:05:20 +0200

    Button styling and testing

--- packages/ui-components/src/Button/__tests__/Button.test.tsx
+++ packages/ui-components/src/Button/__tests__/Button.test.tsx
@@ -1,21 +1,18 @@
 import * as React from "react"
-import { shallow } from "enzyme"
+import { render, mount } from "enzyme"
 
-import { Button, style } from "../Button"
+import Button from "../Button"
 
 describe("Button Component", () => {
   it("Should initialize properly", () => {
-    expect(shallow(<Button>hi</Button>)).toMatchSnapshot()
+    expect(render(<Button>hi</Button>)).toMatchSnapshot()
   })
   it("Should respond to clicks", () => {
-    const myFunc = jest.fn(),
-      button = shallow(<Button onClick={() => myFunc()}>Click me</Button>)
+    const myFunc = jest.fn()
+    const button = mount(<Button onClick={() => myFunc()}>Click me</Button>)
 
     button.simulate("click")
 
     expect(myFunc).toHaveBeenCalled()
-  })
-  it("Should receive proper styles", () => {
-    expect(style({ theme: {} })).toMatchObject({})
   })
 })
