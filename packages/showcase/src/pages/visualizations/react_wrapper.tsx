355902489d13dc11caae8214309c1a7fca02efa7
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2017-09-18 12:17:23 +0200

    Add react wrapper for visualization classes.

---
+++ packages/showcase/src/pages/visualizations/react_wrapper.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import { forEach } from "lodash/fp"
+
+class Wrapper extends React.Component {
+  viz: any
+  containerNode: HTMLElement
+  props: {
+    facade: any
+    accessors?: any
+    data?: any
+    config?: any
+  }
+
+  render() {
+    return <div className="Visualization" ref={containerNode => (this.containerNode = containerNode)} />
+  }
+
+  componentDidMount() {
+    this.viz = new this.props.facade(this.containerNode)
+    this.updateViz()
+    this.viz.draw()
+  }
+
+  componentDidUpdate() {
+    this.updateViz()
+    this.viz.draw()
+  }
+
+  updateViz() {
+    this.viz.data(this.props.data || {})
+    forEach.convert({ cap: false })((accessors: any, key: string): void => {
+      this.viz.accessors(key, accessors)
+    })(this.props.accessors)
+    this.viz.config(this.props.config || {})
+  }
+
+  componentWillUnmount() {
+    this.viz.close()
+  }
+}
+
+export default Wrapper
