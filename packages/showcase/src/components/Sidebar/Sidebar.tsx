e321a968106dcafdf0d4980990aa59cbcf613e20
Peter Szerzo <peterszerzo@Peters-MacBook-Pro.local>
2017-09-21 17:12:41 +0200

    Refactor navigation logic in showcase sidebar

--- packages/showcase/src/components/Sidebar/Sidebar.tsx
+++ packages/showcase/src/components/Sidebar/Sidebar.tsx
@@ -8,59 +8,58 @@ type Props = {
   css: {}
 }
 
-const links = [
+type Link = {
+  label: string
+  links: { url?: string; label: string }[]
+}
+
+const links: Link[] = [
   {
     label: "Basics",
-    links: [
-      { url: "/components/colors", label: "Colors", isEnabled: true },
-      { url: "/components/typography", label: "Typography", isEnabled: true }
-    ]
+    links: [{ url: "/components/colors", label: "Colors" }, { url: "/components/typography", label: "Typography" }]
   },
   {
     label: "Data Entry",
     links: [
-      { url: "/components/buttons", label: "Buttons", isEnabled: true },
-      { url: "/components/switch", label: "Switch", isEnabled: true },
-      { url: "/components/form-fields", label: "Form Fields", isEnabled: true },
-      { url: "/components/color-picker", label: "Color Picker", isEnabled: true },
-      { url: "/components/date-picker", label: "Date Picker", isEnabled: false }
+      { url: "/components/buttons", label: "Buttons" },
+      { url: "/components/switch", label: "Switch" },
+      { url: "/components/form-fields", label: "Form Fields" },
+      { url: "/components/color-picker", label: "Color Picker" },
+      { label: "Date Picker" }
     ]
   },
   {
     label: "Feedback",
     links: [
-      { url: "/components/modals", label: "Modals", isEnabled: false },
-      { url: "/components/notifications", label: "Notifications", isEnabled: false },
-      { url: "/components/alerts", label: "Alerts", isEnabled: false },
-      { url: "/components/messages", label: "Messages", isEnabled: false },
-      { url: "/components/progress", label: "Progress", isEnabled: false },
-      { url: "/components/tooltips", label: "Tooltips", isEnabled: true }
+      { label: "Modals" },
+      { label: "Notifications" },
+      { label: "Alerts" },
+      { label: "Messages" },
+      { label: "Progress" },
+      { url: "/components/tooltips", label: "Tooltips" }
     ]
   },
   {
     label: "UI Elements",
     links: [
-      { url: "/components/context-menu", label: "Context Menu", isEnabled: false },
-      { url: "/components/cards", label: "Cards", isEnabled: true },
-      { url: "/components/chips", label: "Chips", isEnabled: true },
-      { url: "/components/stats", label: "Stats", isEnabled: true }
+      { label: "Context Menu" },
+      { url: "/components/cards", label: "Cards" },
+      { url: "/components/chips", label: "Chips" },
+      { url: "/components/stats", label: "Stats" }
     ]
   },
   {
     label: "Navigation",
     links: [
-      { url: "/components/sidebar", label: "Sidebar", isEnabled: true },
-      { url: "/components/side-navigation", label: "Side Navigation", isEnabled: false },
-      { url: "/components/tabs", label: "Tabs", isEnabled: false },
-      { url: "/components/pagination", label: "Pagination", isEnabled: false }
+      { url: "/components/sidebar", label: "Sidebar" },
+      { label: "Side Navigation" },
+      { label: "Tabs" },
+      { label: "Pagination" }
     ]
   },
   {
     label: "Layout",
-    links: [
-      { url: "/components/grid", label: "Grid", isEnabled: false },
-      { url: "/components/list", label: "List", isEnabled: false }
-    ]
+    links: [{ label: "Grid" }, { label: "List" }]
   }
 ]
 
@@ -70,9 +69,9 @@ export default ({ location, css }: Props) => (
       const isOpen = location && link.links.map(link => link.url).includes(location.pathname)
       return (
         <SidebarItem key={index} open={isOpen} label={link.label}>
-          {link.links.map(({ url, label, isEnabled }, index) => {
+          {link.links.map(({ url, label }, index) => {
             return (
-              <SidebarLink key={index} to={url} disabled={!isEnabled}>
+              <SidebarLink key={index} to={url} disabled={!url}>
                 {label}
               </SidebarLink>
             )
