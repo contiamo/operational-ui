fc66d14876c218df3260ea00b0621dc747947542
Peter Szerzo <peterszerzo@Peters-MacBook-Pro.local>
2017-09-21 10:10:06 +0200

    Fix gitignore

---
+++ packages/showcase/src/pages/Typography/snippets/Typography.snippet.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { contiamoTheme as theme } from "contiamo-ui-components"
+
+export default (
+  <div>
+    <h1 style={theme.typography.heading1}>I am a heading1.</h1>
+    <h2 style={theme.typography.heading2}>I am a heading2.</h2>
+    <p style={theme.typography.body}>I am a regular body section.</p>
+    <p style={theme.typography.small}>I am a little smaller than that.</p>
+  </div>
+)
