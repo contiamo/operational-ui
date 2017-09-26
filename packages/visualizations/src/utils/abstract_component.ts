b70c29f5fdf8e99d4c5cc782f5f1039339487037
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2017-09-26 15:29:44 +0200

    Remove unused abstract component class.

--- packages/visualizations/src/utils/abstract_component.ts
+++
@@ -1,9 +0,0 @@
-import StateHandler from "./state_handler"
-import EventHandler from "./event_handler"
-
-abstract class AbstractComponent {
-  state: StateHandler
-  events: EventHandler
-}
-
-export default AbstractComponent
