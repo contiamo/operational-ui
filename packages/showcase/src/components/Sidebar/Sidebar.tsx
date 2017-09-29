1177bcdafd7ff9f1e90f02592396d91e914e2327
Peter Szerzo <peterszerzo@Peters-MBP.localdomain>
2017-09-29 08:50:55 +0200

    Reorganize showcase side menu

--- packages/showcase/src/components/Sidebar/Sidebar.tsx
+++ packages/showcase/src/components/Sidebar/Sidebar.tsx
@@ -19,43 +19,43 @@ const links: Link[] = [
     links: [{ url: "/components/colors", label: "Colors" }, { url: "/components/typography", label: "Typography" }]
   },
   {
-    label: "Data Entry",
+    label: "UI Elements",
     links: [
       { url: "/components/buttons", label: "Buttons" },
-      { url: "/components/switch", label: "Switch" },
-      { url: "/components/form-fields", label: "Form Fields" },
+      { url: "/components/cards", label: "Cards" },
+      { url: "/components/chips", label: "Chips" },
+      { label: "Context Menu" },
+      { url: "/components/info-tiles", label: "InfoTiles" },
+      { url: "/components/timeline", label: "Timeline" }
+    ]
+  },
+  {
+    label: "Data Entry",
+    links: [
       { url: "/components/color-picker", label: "Color Picker" },
-      { label: "Date Picker" }
+      { label: "Date Picker" },
+      { url: "/components/form-fields", label: "Form Fields" },
+      { url: "/components/switch", label: "Switch" }
     ]
   },
   {
     label: "Feedback",
     links: [
-      { label: "Modals" },
-      { label: "Notifications" },
       { label: "Alerts" },
       { label: "Messages" },
+      { label: "Modals" },
+      { label: "Notifications" },
       { url: "/components/progress", label: "Progress" },
-      { url: "/components/tooltips", label: "Tooltips" },
-      { url: "/components/timeline", label: "Timeline" }
-    ]
-  },
-  {
-    label: "UI Elements",
-    links: [
-      { label: "Context Menu" },
-      { url: "/components/cards", label: "Cards" },
-      { url: "/components/chips", label: "Chips" },
-      { url: "/components/info-tiles", label: "InfoTiles" }
+      { url: "/components/tooltips", label: "Tooltips" }
     ]
   },
   {
     label: "Navigation",
     links: [
+      { url: "/components/paginator", label: "Pagination" },
       { url: "/components/sidebar", label: "Sidebar" },
       { label: "Side Navigation" },
-      { label: "Tabs" },
-      { url: "/components/paginator", label: "Pagination" }
+      { label: "Tabs" }
     ]
   },
   {
