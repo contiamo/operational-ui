a121c4f3e56d52ecdc68fe0f065cc0df7f80f427
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2017-10-10 13:51:02 +0200

    Move viz config and accessors to individual showcase cases.

--- packages/showcase/src/pages/visualizations/ProcessFlow/ProcessFlow.tsx
+++ packages/showcase/src/pages/visualizations/ProcessFlow/ProcessFlow.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as ContiamoVisualizations from "contiamo-visualizations"
 import { Card, CardHeader } from "contiamo-ui-components"
-import * as data from "./data/index"
+import * as caseData from "./data/index"
 
 // @TODO Fix this!
 // import { ProcessFlow, VisualizationWrapper } from "contiamo-visualizations"
@@ -17,46 +17,15 @@ interface IProps {
   }
 }
 
-const config = {
-  arrowFill: "none"
-}
-
-const accessors = {
-  node: {
-    color: (node: any) => {
-      if (node.group === "start") {
-        return "lightgreen"
-      } else if (node.group === "end") {
-        return "lightcoral"
-      }
-    },
-    label: (node: any) => "Node " + node.id,
-    labelPosition: (node: any) => "top",
-    shape: (node: any) => (node.group === "start" ? "square" : node.group === "end" ? "circle" : "diamond"),
-    // size: (node: any) => (node.group === "start" ? 50 : 20),
-    stroke: (node: any) => {
-      if (node.group) {
-        return "none"
-      }
-    }
-  },
-  link: {
-    stroke: (link: any) => {
-      if (link.source.attributes.group === "start") {
-        return "lightgreen"
-      } else if (link.target.attributes.group === "end") {
-        return "lightcoral"
-      }
-    }
-  }
-}
-
 const ProcessFlowContainer: React.SFC<IProps> = props => {
   const case_: string = props.match.params.case
+  const config: any = (caseData as any)[case_].config || {}
+  const accessors: any = (caseData as any)[case_].accessors || {}
+  const data: any = (caseData as any)[case_].data || {}
   return (
     <Card>
       <CardHeader>Process Flow Visualization</CardHeader>
-      <VisualizationWrapper facade={ProcessFlow} data={(data as any)[case_]} accessors={accessors} config={config} />
+      <VisualizationWrapper facade={ProcessFlow} data={data} accessors={accessors} config={config} />
     </Card>
   )
 }
