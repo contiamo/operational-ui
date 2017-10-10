1ae2ecdf9f4828ef9cda874e211cf13d0b8b8dfa
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2017-10-10 14:15:55 +0200

    Add loop handler and corresponding process flow case.

--- packages/showcase/src/pages/visualizations/ProcessFlow/data/index.ts
+++ packages/showcase/src/pages/visualizations/ProcessFlow/data/index.ts
@@ -1,4 +1,5 @@
 import case01 from "./case01"
 import case02 from "./case02"
+import case03 from "./case03"
 
-export { case01, case02 }
+export { case01, case02, case03 }
--- packages/visualizations/index.ts
+++ packages/visualizations/index.ts
@@ -1,5 +1,6 @@
 // Simple imports and exports for consumers of the library.
 import ProcessFlow from "./src/ProcessFlow/facade"
+import ProcessFlowLoopHandler from "./src/utils/process_flow_loop_handler"
 import VisualizationWrapper from "./src/utils/VisualizationWrapper"
 
-export { ProcessFlow, VisualizationWrapper }
+export { ProcessFlow, ProcessFlowLoopHandler, VisualizationWrapper }
