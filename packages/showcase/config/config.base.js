f332c9d3af1aae13128fd5d9f80668b1bd55b7d2
Peter Szerzo <peterszerzo@Peters-MacBook-Pro.local>
2017-10-18 11:11:25 +0200

    Fix showcase webpack build

--- packages/showcase/config/config.base.js
+++ packages/showcase/config/config.base.js
@@ -44,17 +44,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       hash: true,
       template: resolve(__dirname, "..", "public", "index.html")
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      minChunks: function(module) {
-        // This prevents stylesheet resources with the .css or .scss extension
-        // from being moved from their original chunk to the vendor chunk
-        if (module.resource && /^.*\.(css|scss)$/.test(module.resource)) {
-          return false
-        }
-        return module.context && module.context.indexOf("node_modules") !== -1
-      }
     })
   ],
   resolve: {
--- packages/showcase/config/config.production.js
+++ packages/showcase/config/config.production.js
@@ -18,6 +18,7 @@ module.exports = webpackMerge(baseConfig, {
     ]),
     new webpack.DefinePlugin({ 
       "process.env.NODE_ENV": JSON.stringify("production")
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ]
 })
