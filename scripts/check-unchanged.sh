53c93c9308ca64c661ff8593ec5bf8889bdcc51b
peterszerzo <szerzo.peter@gmail.com>
2017-12-13 13:52:06 +0100

    Improve check unchanged script

--- scripts/check-unchanged.sh
+++ scripts/check-unchanged.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -n "$(git status --porcelain | egrep -v '.*(\.|-)lock(\.json)?$')" ]; then
+if [ -n "$(git status --porcelain | egrep -v '((\.lock)|(-lock\.json)|(\.log)|(\.map))$')" ]; then
   echo "changes";
   exit 1;
 else
