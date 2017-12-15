c6f284009dc25f091fdd1338671b8d733e578604
peterszerzo <szerzo.peter@gmail.com>
2017-12-15 15:44:04 +0100

    Setup showcase2 package using next.js

---
+++ packages/showcase2/.next/5.44661a8a8c072abfbdd7.hot-update.js
@@ -0,0 +1,76 @@
+webpackHotUpdate(5,{
+
+/***/ 633:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(__resourceQuery) {
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__(17);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _components = __webpack_require__(634);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/peterszerzo/Documents/contiamo/operational-ui/packages/showcase2/pages/index.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement(_components.ThemeProvider, { theme: _components.operationalTheme, __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, _react2.default.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, "Hello!"), _react2.default.createElement(_components.Button, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, "Good bye")));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkJ1dHRvbiIsIlRoZW1lUHJvdmlkZXIiLCJvcGVyYXRpb25hbFRoZW1lIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsQUFBUyxBQUFRLEFBQWUsQUFFaEM7Ozs7OztrQkFBZSxZQUFBO3lCQUNiLEFBQUMsMkNBQUQsQUFBZSxBQUFPO2dCQUF0QjtrQkFBQSxBQUNFO0FBREY7R0FBQSxrQkFDRSxjQUFBOztnQkFBQTtrQkFBQSxBQUNFO0FBREY7QUFBQSxxQkFDRSxjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsS0FERixBQUNFLEFBQ0EsMkJBQUEsQUFBQzs7Z0JBQUQ7a0JBQUE7QUFBQTtBQUFBLEtBSlMsQUFDYixBQUNFLEFBRUU7QUFKTiIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvcGV0ZXJzemVyem8vRG9jdW1lbnRzL2NvbnRpYW1vL29wZXJhdGlvbmFsLXVpL3BhY2thZ2VzL3Nob3djYXNlMiJ9
+
+ ;(function register() { /* react-hot-loader/webpack */ if (true) { if (typeof __REACT_HOT_LOADER__ === 'undefined') { return; } /* eslint-disable camelcase, no-undef */ var webpackExports = typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__ : module.exports; /* eslint-enable camelcase, no-undef */ if (typeof webpackExports === 'function') { __REACT_HOT_LOADER__.register(webpackExports, 'module.exports', "/Users/peterszerzo/Documents/contiamo/operational-ui/packages/showcase2/pages/index.js"); return; } /* eslint-disable no-restricted-syntax */ for (var key in webpackExports) { /* eslint-enable no-restricted-syntax */ if (!Object.prototype.hasOwnProperty.call(webpackExports, key)) { continue; } var namedExport = void 0; try { namedExport = webpackExports[key]; } catch (err) { continue; } __REACT_HOT_LOADER__.register(namedExport, key, "/Users/peterszerzo/Documents/contiamo/operational-ui/packages/showcase2/pages/index.js"); } } })();
+    (function (Component, route) {
+      if (false) return
+      if (false) return
+
+      var qs = __webpack_require__(84)
+      var params = qs.parse(__resourceQuery.slice(1))
+      if (params.entry == null) return
+
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+/* WEBPACK VAR INJECTION */}.call(exports, "?entry"))
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNS40NDY2MWE4YThjMDcyYWJmYmRkNy5ob3QtdXBkYXRlLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vcGFnZXM/NjRiMmY0MCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBCdXR0b24sIFRoZW1lUHJvdmlkZXIsIG9wZXJhdGlvbmFsVGhlbWUgfSBmcm9tIFwiQG9wZXJhdGlvbmFsL2NvbXBvbmVudHNcIlxuXG5leHBvcnQgZGVmYXVsdCAoKSA9PiAoXG4gIDxUaGVtZVByb3ZpZGVyIHRoZW1lPXtvcGVyYXRpb25hbFRoZW1lfT5cbiAgICA8ZGl2PlxuICAgICAgPGgxPkhlbGxvITwvaDE+XG4gICAgICA8QnV0dG9uPkdvb2QgYnllPC9CdXR0b24+XG4gICAgPC9kaXY+XG4gIDwvVGhlbWVQcm92aWRlcj5cbilcblxuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyAuL3BhZ2VzP2VudHJ5Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7O0FBRUE7QUFDQTs7Ozs7QUFEQTtBQUNBO0FBQUE7QUFDQTtBQURBO0FBQUE7O0FBQ0E7QUFDQTtBQURBO0FBQUE7O0FBQ0E7QUFBQTtBQUFBO0FBQUE7O0FBQ0E7QUFBQTtBQUFBO0FBQUE7QUFKQTs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBIiwic291cmNlUm9vdCI6IiJ9
