3b10b713cf6a41425c7e5fa2d7d6d5cc6a0fe41c
peterszerzo <szerzo.peter@gmail.com>
2017-12-18 14:42:48 +0100

    Finish publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,25 @@
-# Draft
-# @todo: finish
-npm run preversion
-node_modules/.bin/lerna publish --skip-npm
-cd packages/ui-components
-npm publish --registry=https://npm.contiamo.com
+#!/bin/bash
+
+pub() {
+  npm run preversion
+  node_modules/.bin/lerna publish --skip-npm
+  cd packages/utils
+  npm publish
+  cd packages/theme
+  npm publish
+  cd packages/components
+  npm publish
+  cd packages/blocks
+  npm publish
+  cd packages/visualizations
+  npm publish
+}
+
+echo "Are you on a clean, up-to-date master branch, and logged into npm as https://www.npmjs.com/~contiamo?" 
+select master in "Yes" "No"; do
+  case $master in
+    Yes ) pub; break;;
+    No ) exit;;
+  esac
+done
+
