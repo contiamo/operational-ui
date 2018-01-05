99995eb60c56fe1e4bb8a162d6a0ebbcd4b19f24
peterszerzo <szerzo.peter@gmail.com>
2018-01-05 10:13:25 +0100

    Update deploy website script

--- scripts/deploy-website.sh
+++ scripts/deploy-website.sh
@@ -5,9 +5,9 @@ cd ../..
 git branch -D gh-pages
 git checkout --orphan gh-pages
 mv packages/showcase/out/* .
-rm -rf packages lerna.json docs yarn.lock
+rm -rf packages lerna.json docs yarn.lock scripts
 git add .
-git commit -m "Publish showcase page"
+git commit -m "Publish showcase page" --no-verify
 git push -f origin gh-pages
 
 
