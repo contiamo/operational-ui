c3b6ca3aeb10f559be85a05b5a5f3cdafbc19c30
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2018-02-12 15:43:17 +0100

    Add border to tooltip arrow.

--- packages/visualizations/lib/utils/styles.js
+++ packages/visualizations/lib/utils/styles.js
@@ -85,18 +85,26 @@ var focusLegendStyle = {
     "& span.title": {
         paddingRight: "6px"
     },
-    "&::after": {
+    "&::before,::after": {
         content: "''",
         position: "absolute",
         width: 0,
         height: 0
     },
-    "&.above::after": {
-        borderTop: "solid 7px #fff",
-        borderLeft: "solid 7px transparent",
-        borderRight: "solid 7px transparent",
+    "&.above::before,.above::after": {
         top: "100%",
         left: "50%",
+    },
+    "&.above::before": {
+        borderLeft: "solid 8px transparent",
+        borderRight: "solid 8px transparent",
+        borderTop: "solid 8px #cdcdcd",
+        marginLeft: "-8px"
+    },
+    "&.above::after": {
+        borderLeft: "solid 7px transparent",
+        borderRight: "solid 7px transparent",
+        borderTop: "solid 7px #fff",
         marginLeft: "-7px"
     }
 };
