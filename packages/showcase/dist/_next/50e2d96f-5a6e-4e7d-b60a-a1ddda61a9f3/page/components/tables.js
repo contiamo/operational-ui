c33e9507fae861d4be6d8f1964c2603e86bbaa78
peterszerzo <szerzo.peter@gmail.com>
2018-03-09 16:02:12 +0100

    Update showcase

---
+++ packages/showcase/dist/_next/50e2d96f-5a6e-4e7d-b60a-a1ddda61a9f3/page/components/tables.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/components/tables', function() {
+            var comp = module.exports=webpackJsonp([32],{1249:function(e,n,t){e.exports=t(1250)},1250:function(e,n,t){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(n,"__esModule",{value:!0});var l=t(0),r=function(e){if(e&&e.__esModule)return e;var n={};if(null!=e)for(var t in e)Object.prototype.hasOwnProperty.call(e,t)&&(n[t]=e[t]);return n.default=e,n}(l),o=t(9),u=t(213),s=a(u),p=t(212),c=a(p),i=t(12),d=a(i),f=[{name:"columns",description:"Table column headings",defaultValue:"-",type:"(string[])[]",optional:!1},{name:"rows",description:"Table rows as an array of array of cells",defaultValue:"-",type:"(string[])[]",optional:!1}];n.default=function(e){return r.createElement(d.default,{pathname:e.url.pathname},r.createElement(o.Card,null,r.createElement("p",null,"Tables simply render a semantic HTML table structure based on raw data."),r.createElement(o.Heading2Type,null,"Usage"),r.createElement(c.default,{snippet:'\n<Table \n  columns={[ "Name", "Title"]}\n  rows={[\n    [ "Max", "Carpenter" ],\n    [ "Moritz", "Baker" ]\n  ]}\n/>\n',components:{Table:o.Table}}),r.createElement(o.Heading2Type,null,"Props"),r.createElement(s.default,{props:f})))}}},[1249]);
+            return { page: comp.default }
+          })
+        
--- packages/showcase/dist/_next/c43e7e7b-ad86-4c65-a72f-c974c4af4f0e/page/components/tables.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/components/tables', function() {
-            var comp = module.exports=webpackJsonp([31],{1679:function(e,n,t){e.exports=t(1680)},1680:function(e,n,t){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(n,"__esModule",{value:!0});var l=t(0),r=function(e){if(e&&e.__esModule)return e;var n={};if(null!=e)for(var t in e)Object.prototype.hasOwnProperty.call(e,t)&&(n[t]=e[t]);return n.default=e,n}(l),o=t(11),u=t(40),s=a(u),p=t(38),c=a(p),i=t(14),d=a(i),f=[{name:"columns",description:"Table column headings",defaultValue:"-",type:"(string[])[]",optional:!1},{name:"rows",description:"Table rows as an array of array of cells",defaultValue:"-",type:"(string[])[]",optional:!1}];n.default=function(e){return r.createElement(d.default,{pathname:e.url.pathname},r.createElement(o.Card,null,r.createElement("p",null,"Tables simply render a semantic HTML table structure based on raw data."),r.createElement(o.Heading2Type,null,"Usage"),r.createElement(c.default,{snippet:'\n<Table \n  columns={[ "Name", "Title"]}\n  rows={[\n    [ "Max", "Carpenter" ],\n    [ "Moritz", "Baker" ]\n  ]}\n/>\n',components:{Table:o.Table}}),r.createElement(o.Heading2Type,null,"Props"),r.createElement(s.default,{props:f})))}}},[1679]);
-            return { page: comp.default }
-          })
-        
