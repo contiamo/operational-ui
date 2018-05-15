afe79052f00ea7f688936b43232fc9a95fc27efd
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2018-05-15 15:40:35 +0200

    Add charts to website.

--- packages/website/src/Sections/Visualizations/index.ts
+++ packages/website/src/Sections/Visualizations/index.ts
@@ -1,7 +1,8 @@
+import * as charts from "./Charts"
 import * as gauges from "./Gauges"
 import * as pieCharts from "./PieCharts"
 import * as polarAreaCharts from "./PolarAreaCharts"
 import * as processFlows from "./ProcessFlows"
 import * as sunbursts from "./Sunbursts"
 
-export default [gauges, pieCharts, polarAreaCharts, processFlows, sunbursts]
+export default [charts, gauges, pieCharts, polarAreaCharts, processFlows, sunbursts]
