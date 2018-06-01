89ca83a4d9a322d4e8d60e9ce5bd76281c83493d
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2018-06-01 11:08:50 +0200

    Add comments to legend utils.

--- packages/visualizations/src/utils/legend_utils.ts
+++ packages/visualizations/src/utils/legend_utils.ts
@@ -1,11 +1,14 @@
+// Width of legend, rounded up to nearest full pixel
 export const roundedUpWidth = (el: any): number => {
   return Math.ceil(el.getBoundingClientRect().width)
 }
 
+// Height of legend, rounded up to nearest full pixel
 export const roundedUpHeight = (el: any): number => {
   return Math.ceil(el.getBoundingClientRect().height)
 }
 
+// Total width of left and right margins
 export const widthMargin = (el: any): number => {
   if (!el) {
     return 0
@@ -14,6 +17,7 @@ export const widthMargin = (el: any): number => {
   return parseFloat(style.marginLeft) + parseFloat(style.marginRight)
 }
 
+// Total width of left and right padding
 export const widthPadding = (el: any): number => {
   if (!el) {
     return 0
@@ -22,6 +26,7 @@ export const widthPadding = (el: any): number => {
   return parseFloat(style.paddingLeft) + parseFloat(style.paddingRight)
 }
 
+// Total height of top and bottom margins
 export const heightMargin = (el: any): number => {
   if (!el) {
     return 0
@@ -30,6 +35,7 @@ export const heightMargin = (el: any): number => {
   return parseFloat(style.marginTop) + parseFloat(style.marginBottom)
 }
 
+// Total legend width
 export const totalWidth = (el: any): number => {
   if (!el) {
     return 0
@@ -40,6 +46,7 @@ export const totalWidth = (el: any): number => {
   return roundedUpWidth(el) + widthMargin(el) - widthPadding(el) + border
 }
 
+// Total legend height
 export const totalHeight = (el: any): number => {
   if (!el) {
     return 0
