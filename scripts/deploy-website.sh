5dd42b60c483a87cc375d3d27c3a0175fb86c9f9
Fabien BERNARD <fabien0102@hotmail.com>
2018-06-04 16:21:53 +0200

    Build with npm instead of bash

--- scripts/deploy-website.sh
+++ scripts/deploy-website.sh
@@ -1,15 +1,36 @@
 #!/bin/sh
 
-COMMIT_HASH=`git rev-parse --verify HEAD`
-git branch -D gh-pages
-git checkout --orphan gh-pages
-rm -rf scripts docs packages
-cp -a dist/* .
-mv dist/visual-tests .
-mv dist/docs .
-rm -rf dist
-git add .
-git add -f docs/build
-git commit -m "Deploy Commit: ${COMMIT_HASH}" --no-verify
-git push -f origin gh-pages
-git checkout master
+build() {
+  rm -rf dist
+  cd packages/website
+  yarn build
+  cd ../..
+  mv packages/website/dist .
+  cd packages/visual-tests
+  yarn build
+  cd ../..
+  mv packages/visual-tests/dist ./dist/visual-tests
+  cd packages/components
+  yarn docs:build
+  cd ../..
+  cp -r packages/components/styleguide ./dist/docs
+}
+
+deploy() {
+  COMMIT_HASH=`git rev-parse --verify HEAD`
+  git branch -D gh-pages
+  git checkout --orphan gh-pages
+  rm -rf scripts docs packages
+  cp -a dist/* .
+  mv dist/visual-tests .
+  mv dist/docs .
+  rm -rf dist
+  git add .
+  git add -f docs/build
+  git commit -m "Deploy Commit: ${COMMIT_HASH}" --no-verify
+  git push -f origin gh-pages
+  git checkout master
+}
+
+build
+deploy
