857d5efcd45120314ec1810ac0b17bbd8541da91
Fabien BERNARD <fabien0102@hotmail.com>
2018-06-07 12:19:21 +0200

    Fix tslint errors and refactor a litlle bit :angel:

--- packages/visual-tests/src/TestCases/Chart/symbol-3.ts
+++ packages/visual-tests/src/TestCases/Chart/symbol-3.ts
@@ -1,44 +1,43 @@
 import { Chart } from "@operational/visualizations"
 import { MarathonEnvironment } from "../../Marathon"
 
-const between = (min, max) => Math.random() * (max - min + 1) + min
+const between = (min: number, max: number) => Math.random() * (max - min + 1) + min
 
-const dataPoint = country => {
-  const generators = {
-    China: () => {
-      const country = "China"
-      const size = Math.floor(between(0.8, 1.5) * 1e6)
-      const sales = size * between(0.01, 0.015)
-      const profit = sales * between(0.1, 0.2)
-      const store = Math.random() > 0.1 ? "MegaStore" : "Store"
-      return { country, size, sales, profit, store }
-    },
-    Germany: () => {
-      const country = "Germany"
-      const size = Math.floor(between(0.2, 1) * 1e6)
-      const sales = size * between(0.05, 0.3)
-      const profit = sales * between(0.3, 0.5)
-      const store = Math.random() > 0.3 ? "MegaStore" : "Store"
-      return { country, size, sales, profit, store }
-    },
-    Netherlands: () => {
-      const country = "Netherlands"
-      const size = Math.floor(between(0.5, 0.8) * 1e6)
-      const sales = size * between(0.1, 0.4)
-      const profit = sales * between(0.4, 0.55)
-      const store = Math.random() > 0.3 ? "MegaStore" : "Store"
-      return { country, size, sales, profit, store }
-    },
-  }
+const generators = {
+  China: () => {
+    const country = "China"
+    const size = Math.floor(between(0.8, 1.5) * 1e6)
+    const sales = size * between(0.01, 0.015)
+    const profit = sales * between(0.1, 0.2)
+    const store = Math.random() > 0.1 ? "MegaStore" : "Store"
+    return { country, size, sales, profit, store }
+  },
+  Germany: () => {
+    const country = "Germany"
+    const size = Math.floor(between(0.2, 1) * 1e6)
+    const sales = size * between(0.05, 0.3)
+    const profit = sales * between(0.3, 0.5)
+    const store = Math.random() > 0.3 ? "MegaStore" : "Store"
+    return { country, size, sales, profit, store }
+  },
+  Netherlands: () => {
+    const country = "Netherlands"
+    const size = Math.floor(between(0.5, 0.8) * 1e6)
+    const sales = size * between(0.1, 0.4)
+    const profit = sales * between(0.4, 0.55)
+    const store = Math.random() > 0.3 ? "MegaStore" : "Store"
+    return { country, size, sales, profit, store }
+  },
+}
+
+type Country = keyof typeof generators
+
+const dataPoint = (country: Country) => {
   return generators[country]()
 }
 
-const generateData = (country, amount = 100) => {
-  let result = []
-  for (let i = 0; i < amount; i++) {
-    result.push(dataPoint(country))
-  }
-  return result
+const generateData = (country: Country, amount = 100) => {
+  return [...Array(amount).fill(0)].map(i => dataPoint(country))
 }
 
 const renderer = {
@@ -48,7 +47,7 @@ const renderer = {
     size: (series: any, d: any) => {
       const range = [5, 80]
       const domain = [0, 1.5 * 1e6]
-      return range[0] + (range[1] - range[0]) * (d.size - domain[0]) / (domain[1] - domain[0])
+      return range[0] + ((range[1] - range[0]) * (d.size - domain[0])) / (domain[1] - domain[0])
     },
     stroke: () => "transparent",
     symbol: (series: any, d: any) => (d.store === "MegaStore" ? "cross" : "diamond"),
@@ -93,16 +92,6 @@ const createData = () => {
       },
     },
   }
-}
-
-const symbols = {
-  1: "circle",
-  2: "cross",
-  3: "diamond",
-  4: "square",
-  5: "squareDiamond",
-  6: "star",
-  7: "triangle",
 }
 
 export const marathon = ({ test, afterAll, container }: MarathonEnvironment): void => {
