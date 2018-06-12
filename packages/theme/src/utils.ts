6d435668d24c1f4d7931c00d1e6725f4b5f83bde
Tejas Kumar <tejas@contiamo.com>
2018-06-12 17:54:33 +0200

    Update style constants

---
+++ packages/theme/src/utils.ts
@@ -0,0 +1,18 @@
+import { Theme } from "./index"
+
+/*
+ * Expands a color expressed either as a custom hex value
+ * or a color key to pick from within the theme.colors object.
+ */
+export const expandColor = (theme: Theme, color?: string): string | null => {
+  if (!color) {
+    return null
+  }
+  const hexRegEx = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)|currentColor/i
+  const isHex = hexRegEx.test(color)
+  if (isHex) {
+    return color
+  }
+  // || null is necessary to coerce undefineds into nulls
+  return (theme.colors as any)[color] || (null as string | null)
+}
