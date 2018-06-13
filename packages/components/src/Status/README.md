8150c52585f9eb448b98dd949a525895c1c2d285
Tejas Kumar <tejas@contiamo.com>
2018-06-13 17:29:07 +0200

    Add Status component

---
+++ packages/components/src/Status/README.md
@@ -0,0 +1,17 @@
+This component displays an indicator of status.
+
+### Usage
+
+```jsx
+<div style={{ maxWidth: 200 }}>
+  <Card>
+    <Status success />Deployed
+  </Card>
+  <Card>
+    <Status error />Offline
+  </Card>
+  <Card>
+    <Status />Neutral
+  </Card>
+</div>
+```
--- packages/components/src/types.ts
+++ packages/components/src/types.ts
@@ -1,9 +1,10 @@
 import { Theme } from "@operational/theme"
 import { CSSProperties } from "glamorous"
+import { Interpolation } from "create-emotion"
 
 export type WithTheme = { theme: Theme }
 
-export type Css = CSSProperties | (<T>(props: T & WithTheme) => CSSProperties)
+export type Css = any
 
 // TODO: make this stricter to disallow { margin: 20, fruit: "much much" }
 export type CssStatic = CSSProperties
