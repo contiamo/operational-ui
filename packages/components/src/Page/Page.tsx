18c08b131197057d57254a4174af75144baa6700
Fabien BERNARD <fabien0102@hotmail.com>
2018-06-19 14:52:32 +0200

    Simple page case

--- packages/components/src/Page/Page.tsx
+++ packages/components/src/Page/Page.tsx
@@ -1,19 +1,15 @@
 import * as React from "react"
 import styled from "react-emotion"
 import { OperationalStyleConstants, Theme } from "@operational/theme"
-import { Icon, IconName } from ".."
+import { Icon, IconName, Title } from ".."
 import PageArea from "../PageArea/PageArea"
 import { CssStatic } from "../types"
 
 export interface Props {
   /** Page title */
   title: string
-  /** Icon displayed next to the title. Should match related sidenav icons */
-  titleIcon?: IconName
-  /** Page breadcrumbs, using the `Breadcrumbs` component */
-  breadcrumbs?: React.ReactNode
-  /** Page controls, typically `condensed button` component inside a fragment */
-  controls?: React.ReactNode
+  /** Page actions, typically `condensed button` component inside a fragment */
+  actions?: React.ReactNode
   /** Content of the page */
   children?: React.ReactNode
   /** Areas template for `PageArea` disposition */
@@ -22,74 +18,15 @@ export interface Props {
   fill?: boolean
 }
 
-const Container = styled("div")(
-  ({
-    theme,
-  }: {
-    theme?: OperationalStyleConstants & {
-      deprecated: Theme
-    }
-  }): CssStatic => ({
-    label: "page",
-    backgroundColor: theme.deprecated.colors.white,
-    padding: `0px ${theme.deprecated.spacing * 1.5}px ${theme.deprecated.spacing * 1.5}px`,
-  }),
-)
+const Container = styled("div")(({ theme }: { theme?: OperationalStyleConstants }) => ({
+  backgroundColor: theme.color.background.lighter,
+}))
 
-const TopBar = styled("div")(
-  ({
-    theme,
-  }: {
-    theme?: OperationalStyleConstants & {
-      deprecated: Theme
-    }
-  }): CssStatic => ({
-    height: theme.deprecated.box,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-  }),
-)
-
-const TitleBar = styled("div")(
-  ({
-    theme,
-  }: {
-    theme?: OperationalStyleConstants & {
-      deprecated: Theme
-    }
-  }): CssStatic => ({
-    ...theme.deprecated.typography.title,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    marginTop: 0.5 * theme.deprecated.spacing,
-    marginBottom: 2 * theme.deprecated.spacing,
-    "& svg": {
-      width: theme.deprecated.spacing * 1.75,
-      height: theme.deprecated.spacing * 1.75,
-      marginRight: theme.deprecated.spacing * 0.5,
-    },
-  }),
-)
-
-const ControlsContainer = styled("div")(
-  ({
-    theme,
-  }: {
-    theme?: OperationalStyleConstants & {
-      deprecated: Theme
-    }
-  }): CssStatic => ({
-    marginLeft: theme.deprecated.spacing,
-    // Offset the line-height setting inherited from being inside a
-    // typography component/mixin.
-    lineHeight: 1,
-    "& > :last-child": {
-      marginRight: 0,
-    },
-  }),
-)
+const TitleBar = styled("div")(({ theme }: { theme?: OperationalStyleConstants }) => ({
+  backgroundColor: theme.color.primary,
+  display: "flex",
+  alignItems: "center",
+}))
 
 const Grid = styled("div")(
   (props: { children?: React.ReactNode; fill?: boolean; theme?: OperationalStyleConstants }) => {
@@ -103,6 +40,7 @@ const Grid = styled("div")(
       maxWidth: props.fill ? "none" : 1150,
       minWidth: 800,
       width: "100%",
+      padding: props.theme.space.element,
     }
   },
 )
@@ -112,13 +50,12 @@ const Page = (props: Props) => {
 
   return (
     <Container>
-      <TopBar>{props.breadcrumbs}</TopBar>
-      <TitleBar>
-        {props.titleIcon &&
-          (props.titleIcon === String(props.titleIcon) ? <Icon name={props.titleIcon} /> : props.titleIcon)}
-        {props.title}
-        <ControlsContainer>{props.controls}</ControlsContainer>
-      </TitleBar>
+      {props.title && (
+        <TitleBar>
+          <Title color="white">{props.title}</Title>
+          {props.actions}
+        </TitleBar>
+      )}
       <Grid fill={props.fill}>{onlyOneChild ? <PageArea>{props.children}</PageArea> : props.children}</Grid>
     </Container>
   )
--- packages/components/src/Page/README.md
+++ packages/components/src/Page/README.md
@@ -7,26 +7,17 @@ This component is typically used inside a layout component along with a sidenav.
 Here is a simple usage example:
 
 ```jsx
-const  Breadcrumb = require("../Breadcrumb/Breadcrumb").default
+;<Page title="Settings Page">
+  <Card>Hello, this is page content</Card>
+</Page>
+```
 
-const breadcrumbs = (
-  <Breadcrumbs>
-    <Breadcrumb>
-      <a>Link One</a>
-    </Breadcrumb>
-    <Breadcrumb>Link Two</Breadcrumb>
-  </Breadcrumbs>
-)
+### With actions
 
-/* Always use condensed buttons in page controls */
-const controls = (<Button condensed>Go somewhere else</Button>);
-
-<Page
-  title="Settings Page"
-  titleIcon="Settings"
-  breadcrumbs={breadcrumbs}
-  controls={controls}
->
-  <p>Hello, this is page content</p>
+```jsx
+/* Always use condensed buttons in page actions */
+const actions = <Button condensed>Go somewhere else</Button>
+;<Page title="Settings Page" actions={actions}>
+  <Card>Hello, this is page content</Card>
 </Page>
 ```
