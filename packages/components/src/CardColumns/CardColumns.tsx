08682f93b5e168953a0f31c057b15425a03e8d40
Tejas Kumar <tejas@contiamo.com>
2018-06-20 15:51:25 +0200

    [Patch] Export of CardColumns

--- packages/components/src/CardColumns/CardColumns.tsx
+++ packages/components/src/CardColumns/CardColumns.tsx
@@ -2,13 +2,17 @@ import * as React from "react"
 import styled from "react-emotion"
 import { OperationalStyleConstants } from "@operational/theme"
 
-const CardColumns = styled("div")(({ children, theme }: React.Props<{}> & { theme?: OperationalStyleConstants }) => ({
-  display: "flex",
-  flexWrap: "wrap",
-  margin: -(theme.space.element / 2),
-  "& > *": {
-    flexBasis: `${React.Children.count(children)}%`,
-  },
-}))
+const StyledCardColumns = styled("div")(
+  ({ children, theme }: React.Props<{}> & { theme?: OperationalStyleConstants }) => ({
+    display: "flex",
+    flexWrap: "wrap",
+    margin: -(theme.space.element / 2),
+    "& > *": {
+      flexBasis: `${React.Children.count(children)}%`,
+    },
+  }),
+)
 
-export default (props: {}) => <CardColumns {...props} />
+const CardColumns: React.SFC = props => <StyledCardColumns {...props} />
+
+export default CardColumns
