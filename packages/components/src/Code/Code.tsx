3f80ae63aad32e83e9be44ba409366c240b07222
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2018-06-20 11:24:37 +0200

    Initial code component commit.

---
+++ packages/components/src/Code/Code.tsx
@@ -0,0 +1,46 @@
+import * as React from "react"
+import styled from "react-emotion"
+import { OperationalStyleConstants } from "@operational/theme"
+import { Css } from "../types"
+import * as hljs from "highlight.js"
+
+export interface Props {
+  /** Id */
+  id?: string
+
+  /** `css` prop as expected in a glamorous component */
+  css?: Css
+
+  /** Language for syntax highlighting */
+  language?: string
+
+  className?: string
+
+  children?: any
+}
+
+const Container = styled("code")(
+  ({ theme }: { theme?: OperationalStyleConstants }): {} => {
+    return {
+      border: "1px solid rgba(20, 153, 206, 0.1)",
+      borderRadius: theme.borderRadius,
+      overflow: "hidden",
+      padding: theme.space.small,
+      backgroundColor: "rgba(20, 153, 206, 0.05)",
+      minWidth: 500,
+      fontSize: theme.font.size.body,
+      fontFamily: theme.font.family.code,
+    }
+  },
+)
+
+const Code = (props: Props) => {
+  hljs.initHighlightingOnLoad()
+  return (
+    <pre>
+      <Container className={props.language}>{props.children}</Container>
+    </pre>
+  )
+}
+
+export default Code
---
+++ packages/components/src/Code/README.md
@@ -0,0 +1,20 @@
+Box for displaying formatted code snippets.
+
+### Usage
+
+```jsx
+<Code language="json">
+  {`{
+  "properties": {
+    "startAt": {
+      "type": "string",
+      "format": "date-time"
+    },
+    "endAt": {
+      "type": "string",
+      "format": "date-time"
+    }
+  }
+}`}
+</Code>
+```
