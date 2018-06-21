1dd6014bc8b421bb3c226f81bb2c47e2a6d4bf90
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2018-06-21 13:13:51 +0200

    Extract global styles from Code component.

--- packages/components/src/Code/Code.tsx
+++ packages/components/src/Code/Code.tsx
@@ -3,6 +3,9 @@ import styled from "react-emotion"
 import { OperationalStyleConstants } from "@operational/theme"
 import Highlight from "react-highlight"
 import { injectGlobal } from "emotion"
+import styles from "./global_styles"
+
+injectGlobal`${styles}`
 
 export interface Props {
   /** Id */
@@ -33,77 +36,3 @@ const Code = (props: Props) => (
 )
 
 export default Code
-
-injectGlobal`
-/*
-This stylesheet must be injected into the document in order for the syntax highlighting to work
-Direct copy from https://github.com/isagalaev/highlight.js/blob/master/src/styles/vs.css
-*/
-
-/*
-Visual Studio-like style based on original C# coloring by Jason Diamond <jason@diamond.name>
-*/
-.hljs {
-  display: block;
-  overflow-x: auto;
-  padding: 0.5em;
-  background: white;
-  color: black;
-}
-
-.hljs-comment,
-.hljs-quote,
-.hljs-variable {
-  color: #008000;
-}
-
-.hljs-keyword,
-.hljs-selector-tag,
-.hljs-built_in,
-.hljs-name,
-.hljs-tag {
-  color: #00f;
-}
-
-.hljs-string,
-.hljs-title,
-.hljs-section,
-.hljs-attribute,
-.hljs-literal,
-.hljs-template-tag,
-.hljs-template-variable,
-.hljs-type,
-.hljs-addition {
-  color: #a31515;
-}
-
-.hljs-deletion,
-.hljs-selector-attr,
-.hljs-selector-pseudo,
-.hljs-meta {
-  color: #2b91af;
-}
-
-.hljs-doctag {
-  color: #808080;
-}
-
-.hljs-attr {
-  color: #f00;
-}
-
-.hljs-symbol,
-.hljs-bullet,
-.hljs-link {
-  color: #00b0e8;
-}
-
-
-.hljs-emphasis {
-  font-style: italic;
-}
-
-.hljs-strong {
-  font-weight: bold;
-}
-`
