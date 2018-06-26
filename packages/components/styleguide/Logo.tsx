7515dcfc71cf0733380e497dfe261de6f6b8b301
peterszerzo <szerzo.peter@gmail.com>
2018-06-26 16:15:16 +0200

    Fix styleguide banner z-index bug with portals

--- packages/components/styleguide/Logo.tsx
+++ packages/components/styleguide/Logo.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { createPortal } from "react-dom"
 import styled from "react-emotion"
 import marked from "marked"
 
@@ -10,6 +11,7 @@ export interface Props {}
 
 export interface State {
   isOpen: boolean
+  isPortalRendered: boolean
   rotation: number
 }
 
@@ -22,7 +24,6 @@ const Container = styled("div")`
   bottom: 0px;
   padding: 10px;
   background-color: #005f96;
-  background-clip: content-box;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -79,63 +80,98 @@ const LogoType = styled("h1")`
   font-size: 20px;
   font-weight: 400;
   margin: 0px;
+  cursor: pointer;
   & > * {
     display: inline-block;
     vertical-align: middle;
   }
 `
 
+const operationalBannerContainerId = "operational-banner-container"
+
 class Logo extends React.Component<Props, State> {
   state = {
-    isOpen: true,
+    isOpen: !window.location.hash,
+    isPortalRendered: false,
     rotation: 0,
   }
 
+  rotationInterval: number
+
   componentDidMount() {
-    setInterval(() => {
+    if (!document.querySelector(`#${operationalBannerContainerId}`)) {
+      const operationalBannerContainer = document.createElement("div")
+      operationalBannerContainer.setAttribute("id", operationalBannerContainerId)
+      document.body.appendChild(operationalBannerContainer)
+      this.setState(prevState => ({
+        isPortalRendered: true,
+      }))
+    } else {
+      if (!this.state.isPortalRendered) {
+        this.setState(prevState => ({
+          isPortalRendered: true,
+        }))
+      }
+    }
+    this.rotationInterval = window.setInterval(() => {
       this.setState(prevState => ({
         rotation: 180 - prevState.rotation,
       }))
     }, 8000)
   }
 
+  componentWillUnmount() {
+    window.clearInterval(this.rotationInterval)
+  }
+
   render() {
     return (
       <OperationalUI>
-        {this.state.isOpen ? (
-          <Container>
-            <Animation size={Math.max(window.innerHeight, window.innerWidth)} />
-            <Content>
-              <TitleBar>
-                <OperationalLogo size={110} rotation={this.state.rotation} />
-                <TitleBarContent>
-                  <h1>Operational UI</h1>
-                  <div>
-                    <Button
-                      onClick={() => {
-                        this.setState(prevState => ({
-                          isOpen: !prevState.isOpen,
-                        }))
-                      }}
-                    >
-                      Docs
-                    </Button>
-                    <Button>GitHub</Button>
-                  </div>
-                </TitleBarContent>
-              </TitleBar>
-              <Static
-                dangerouslySetInnerHTML={{
-                  __html: marked(`Operational is a UI library optimized for day-to-day operational decision-making. It does its best when used for interfaces that assume familiarity through routine use, prioritizing compactness and [small effective differences](https://twitter.com/edwardtufte/status/450076034759524352).
+        {this.state.isOpen && this.state.isPortalRendered ? (
+          createPortal(
+            <Container>
+              <Animation size={Math.max(window.innerHeight, window.innerWidth)} />
+              <Content>
+                <TitleBar>
+                  <OperationalLogo size={110} rotation={this.state.rotation} />
+                  <TitleBarContent>
+                    <h1>Operational UI</h1>
+                    <div>
+                      <Button
+                        onClick={() => {
+                          window.location.hash = "docs-home"
+                          this.setState(prevState => ({
+                            isOpen: !prevState.isOpen,
+                          }))
+                        }}
+                      >
+                        Docs
+                      </Button>
+                      <Button>GitHub</Button>
+                    </div>
+                  </TitleBarContent>
+                </TitleBar>
+                <Static
+                  dangerouslySetInnerHTML={{
+                    __html: marked(`Operational is a UI library optimized for day-to-day operational decision-making. It does its best when used for interfaces that assume familiarity through routine use, prioritizing compactness and [small effective differences](https://twitter.com/edwardtufte/status/450076034759524352).
 
 It is predictable to use, and it lets you and your team breathe. Exhales, not sighs.
               `),
-                }}
-              />
-            </Content>
-          </Container>
+                  }}
+                />
+              </Content>
+            </Container>,
+            document.querySelector(`#${operationalBannerContainerId}`),
+          )
         ) : (
-          <LogoType>
+          <LogoType
+            onClick={() => {
+              window.location.hash = ""
+              this.setState(prevState => ({
+                isOpen: true,
+              }))
+            }}
+          >
             <Icon name="OperationalUI" size={28} left />
             <span>Operational UI</span>
           </LogoType>
