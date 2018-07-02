5a02768a03a1a62d2c7fb8e5cf5b54c9381b2246
peterszerzo <szerzo.peter@gmail.com>
2018-07-02 11:54:50 +0200

    Message management inside OperationalUI

--- packages/components/src/OperationalUI/OperationalUI.tsx
+++ packages/components/src/OperationalUI/OperationalUI.tsx
@@ -5,6 +5,9 @@ import { ThemeProvider } from "emotion-theming"
 import constants, { OperationalStyleConstants } from "../utils/constants"
 import { darken } from "@operational/utils"
 
+import Messages from "../Messages/Messages"
+import Message from "../Message/Message"
+
 export interface Props {
   /** Children */
   children?: React.ReactNode
@@ -14,6 +17,36 @@ export interface Props {
   pushState?: (path: string) => void
   /** Custom replace state method expecting a single string */
   replaceState?: (path: string) => void
+  /**
+   * A time interval after which a non-error message should be automatically dismissed, measured in milliseconds.
+   *
+   * @default 10000 (10s)
+   */
+  hideMessageAfter?: number
+}
+
+type MessageType = "info" | "success" | "error"
+
+export interface State {
+  messages: {
+    message: {
+      body: string
+      type: MessageType
+    }
+    addedAt: number
+  }[]
+  now: number
+}
+
+const colorByMessageType = (type: MessageType): string => {
+  switch (type) {
+    case "info":
+      return "rgba(20, 153, 206, 0.9)"
+    case "success":
+      return "rgba(13, 170, 31, 0.9)"
+    case "error":
+      return "rgba(154, 0, 0, 0.9)"
+  }
 }
 
 const { Provider, Consumer } = React.createContext({})
@@ -50,9 +83,25 @@ a:hover: {
 }
 `
 
-class OperationalUI extends React.Component<Props, {}> {
+class OperationalUI extends React.Component<Props, State> {
+  state: State = {
+    messages: [],
+    now: new Date().getTime(),
+  }
+
+  timer: number
+
   componentDidMount() {
     this.props.withBaseStyles && injectGlobal(baseStylesheet(constants))
+    this.timer = window.setInterval(() => {
+      this.setState(prevState => ({
+        now: new Date().getTime(),
+      }))
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timer)
   }
 
   render() {
@@ -63,9 +112,36 @@ class OperationalUI extends React.Component<Props, {}> {
           value={{
             pushState,
             replaceState,
+            pushMessage: (message: { body: string; type: MessageType }) => {
+              this.setState(prevState => ({
+                messages: [...prevState.messages, { message, addedAt: new Date().getTime() }],
+              }))
+            },
           }}
         >
-          {children}
+          <>
+            <Messages>
+              {this.state.messages.map(
+                ({ message, addedAt }, index) =>
+                  this.state.now - addedAt < (this.props.hideMessageAfter || 10000) && (
+                    <Message
+                      key={index}
+                      color={colorByMessageType(message.type)}
+                      onClose={() => {
+                        this.setState(prevState => ({
+                          messages: prevState.messages.filter(
+                            (message, filteredMessageIndex) => filteredMessageIndex !== index,
+                          ),
+                        }))
+                      }}
+                    >
+                      {message.body}
+                    </Message>
+                  ),
+              )}
+            </Messages>
+            {children}
+          </>
         </Provider>
       </ThemeProvider>
     )
