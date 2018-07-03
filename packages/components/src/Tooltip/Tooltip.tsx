cb6b2ec2b3f392c8f656d74dc55bd2493030ddf8
peterszerzo <szerzo.peter@gmail.com>
2018-07-03 11:47:10 +0200

    Refactor position-based styles on tooltip

--- packages/components/src/Tooltip/Tooltip.tsx
+++ packages/components/src/Tooltip/Tooltip.tsx
@@ -37,6 +37,71 @@ export interface State {
 
 type Position = "top" | "left" | "right" | "bottom"
 
+const containerPositionStyles = (position: Position): {} => {
+  switch (position) {
+    case "top":
+      return {
+        left: "50%",
+        transform: "translate3d(-50%, calc(-100% - 6px), 0)",
+      }
+    case "bottom":
+      return {
+        left: "50%",
+        top: "100%",
+        transform: "translate3d(-50%, 6px, 0)",
+      }
+    case "left":
+      return {
+        top: "50%",
+        left: -6,
+        transform: "translate3d(-100%, -50%, 0)",
+      }
+    case "right":
+      return {
+        top: "50%",
+        right: -6,
+        transform: "translate3d(100%, -50%, 0)",
+      }
+  }
+}
+
+const pointerTrianglePositionStyles = (position: Position, backgroundColor: string): {} => {
+  switch (position) {
+    case "top":
+      return {
+        bottom: -4,
+        left: `calc(50% - 6px)`,
+        borderLeft: "6px solid transparent",
+        borderRight: "6px solid transparent",
+        borderTop: `6px solid ${backgroundColor}`,
+      }
+    case "bottom":
+      return {
+        top: -4,
+        left: `calc(50% - 6px)`,
+        borderLeft: "6px solid transparent",
+        borderRight: "6px solid transparent",
+        borderBottom: `6px solid ${backgroundColor}`,
+      }
+    case "left":
+      return {
+        right: -4,
+        top: `calc(50% - 6px)`,
+        borderTop: "6px solid transparent",
+        borderBottom: "6px solid transparent",
+        borderLeft: `6px solid ${backgroundColor}`,
+      }
+    case "right":
+      return {
+        left: -4,
+        top: `calc(50% - 6px)`,
+        borderTop: "6px solid transparent",
+        borderBottom: "6px solid transparent",
+        borderRight: `6px solid ${backgroundColor}`,
+      }
+  }
+}
+
 const Container = styled("div")(
   ({
     position,
@@ -81,40 +146,7 @@ const Container = styled("div")(
             // Otherwise, honor the single line text width unless greater than 150px.
             width: singleLineTextWidth === 0 ? 150 : Math.min(singleLineTextWidth + 4, 150),
           }),
-      ...(() => {
-        if (position === "top") {
-          return {
-            left: "50%",
-            transform: "translate3d(-50%, calc(-100% - 6px), 0)",
-          }
-        }
-
-        if (position === "bottom") {
-          return {
-            left: "50%",
-            top: "100%",
-            transform: "translate3d(-50%, 6px, 0)",
-          }
-        }
-
-        if (position === "left") {
-          return {
-            top: "50%",
-            left: -6,
-            transform: "translate3d(-100%, -50%, 0)",
-          }
-        }
-
-        if (position === "right") {
-          return {
-            top: "50%",
-            right: -6,
-            transform: "translate3d(100%, -50%, 0)",
-          }
-        }
-
-        return {}
-      })(),
+      ...containerPositionStyles(position),
       // This pseudo-element extends the clickable area of the far-away tooltip.
       "&::after": {
         content: "''",
@@ -132,49 +164,7 @@ const Container = styled("div")(
         zIndex: theme.deprecated.baseZIndex - 1,
         width: 0,
         height: 0,
-        ...(() => {
-          if (position === "top") {
-            return {
-              bottom: -4,
-              left: `calc(50% - 6px)`,
-              borderLeft: "6px solid transparent",
-              borderRight: "6px solid transparent",
-              borderTop: `6px solid ${backgroundColor}`,
-            }
-          }
-
-          if (position === "bottom") {
-            return {
-              top: -4,
-              left: `calc(50% - 6px)`,
-              borderLeft: "6px solid transparent",
-              borderRight: "6px solid transparent",
-              borderBottom: `6px solid ${backgroundColor}`,
-            }
-          }
-
-          if (position === "left") {
-            return {
-              right: -4,
-              top: `calc(50% - 6px)`,
-              borderTop: "6px solid transparent",
-              borderBottom: "6px solid transparent",
-              borderLeft: `6px solid ${backgroundColor}`,
-            }
-          }
-
-          if (position === "right") {
-            return {
-              left: -4,
-              top: `calc(50% - 6px)`,
-              borderTop: "6px solid transparent",
-              borderBottom: "6px solid transparent",
-              borderRight: `6px solid ${backgroundColor}`,
-            }
-          }
-
-          return {}
-        })(),
+        ...pointerTrianglePositionStyles(position, backgroundColor),
       },
     }
   },
