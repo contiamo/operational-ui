ff2336377b8dabb6f827cf2aa24213017e62cd33
peterszerzo <szerzo.peter@gmail.com>
2018-07-04 11:14:18 +0200

    Refactor and optimize message management

--- packages/components/src/OperationalUI/OperationalUI.tsx
+++ packages/components/src/OperationalUI/OperationalUI.tsx
@@ -7,7 +7,7 @@ import { darken } from "@operational/utils"
 import { MessageType, Context } from "../types"
 
 import Messages from "../Messages/Messages"
-import Message from "../Message/Message"
+import MessageComponent from "../Message/Message"
 
 export interface Props {
   /** Children */
@@ -26,12 +26,14 @@ export interface Props {
   hideMessageAfter?: number
 }
 
+export interface Message {
+  body: string
+  type: MessageType
+}
+
 export interface State {
   messages: {
-    message: {
-      body: string
-      type: MessageType
-    }
+    message: Message
     addedAt: number
   }[]
 }
@@ -53,7 +55,7 @@ const colorByMessageType = (type: MessageType): string => {
 const defaultContext: Context = {
   pushState: undefined,
   replaceState: undefined,
-  pushMessage: (message: { body: string; type: MessageType }) => {},
+  pushMessage: (message: Message) => {},
 }
 
 const { Provider, Consumer } = React.createContext(defaultContext)
@@ -95,17 +97,21 @@ class OperationalUI extends React.Component<Props, State> {
     messages: [],
   }
 
-  timer: number | null = null
+  // The interval responsible for periodically checking whether any messages need to be removed from state
+  messageTimerInterval: number | null = null
 
   removeOutdatedMessages() {
     if (this.props.hideMessageAfter === 0) {
       return
     }
     this.setState(prevState => {
+      // Work out the new list of messages, filtering out the ones that are outdated
+      // ( outdated = not of type `error`, and has been around longer than `this.props.hideMessageAfter`, defaulting to 10s )
       const now = new Date().getTime()
       const filteredMessages = prevState.messages.filter(
         ({ message, addedAt }) => message.type === "error" || now - addedAt < (this.props.hideMessageAfter || 10000),
       )
+      // Only run a setState if any message(s) were removed. Otherwise, this method returns `undefined` and the component is not updated at all.
       if (prevState.messages.length > filteredMessages.length) {
         return {
           messages: filteredMessages,
@@ -114,18 +120,43 @@ class OperationalUI extends React.Component<Props, State> {
     })
   }
 
+  startMessagesTimer() {
+    if (this.messageTimerInterval !== null) {
+      return
+    }
+    // A broad interval of 2 seconds is chosen so messages created around the same time disappear at the same time, avoiding the disorienting effect of tiny time delays.
+    this.messageTimerInterval = window.setInterval(() => {
+      this.removeOutdatedMessages()
+    }, 2000)
+  }
+
+  stopMessagesTimer() {
+    window.clearInterval(this.messageTimerInterval)
+    // Clear the message interval instance in order to prevent multiple timers being accidentally created. Do not remove this assignment.
+    this.messageTimerInterval = null
+  }
+
   componentDidMount() {
     this.props.withBaseStyles && injectGlobal(baseStylesheet(constants))
   }
 
-  startMessagesTimer() {
-    this.timer = window.setInterval(() => {
-      this.removeOutdatedMessages()
-    }, 1000)
+  componentWillUnmount() {
+    window.clearInterval(this.messageTimerInterval)
   }
 
-  componentWillUnmount() {
-    window.clearInterval(this.timer)
+  /*
+   * Stop and start the messages timer depending on whether it's needed or not.
+   * CAUTION: this logic may result in very rare edge cases with React's render loop if two messages are removed within the same frame, causing timers to occasionally not stop or restart.
+   * Since the consequences of this are not catastrophic for UX, we have decided to leave this optimization in place. Simply forgo the optimization and keep the timer going throughout the lifecycle of this component if more safety is desired. 
+   */
+  componentDidUpdate(_: Props, prevState: State) {
+    if (this.state.messages.length === 0 && prevState.messages.length > 0) {
+      this.stopMessagesTimer()
+      return
+    }
+    if (prevState.messages.length === 0 && this.state.messages.length > 0) {
+      this.startMessagesTimer()
+    }
   }
 
   render() {
@@ -137,11 +168,8 @@ class OperationalUI extends React.Component<Props, State> {
             pushState,
             replaceState,
             pushMessage: (message: { body: string; type: MessageType }) => {
-              if (this.timer === null) {
-                this.startMessagesTimer()
-              }
               this.setState(prevState => ({
-                messages: [...prevState.messages, { message, addedAt: new Date().getTime() }],
+                messages: [{ message, addedAt: new Date().getTime() }, ...prevState.messages],
               }))
             },
           }}
@@ -149,7 +177,7 @@ class OperationalUI extends React.Component<Props, State> {
           <>
             <Messages>
               {this.state.messages.map(({ message }, index) => (
-                <Message
+                <MessageComponent
                   key={index}
                   color={colorByMessageType(message.type)}
                   onClose={() => {
@@ -159,7 +187,7 @@ class OperationalUI extends React.Component<Props, State> {
                   }}
                 >
                   {message.body}
-                </Message>
+                </MessageComponent>
               ))}
             </Messages>
             {children}
