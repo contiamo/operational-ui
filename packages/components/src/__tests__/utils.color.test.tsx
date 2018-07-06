453c138d1a57b67d9bd763d19bfc7774d881b51c
Michael Franzkowiak <michael@contiamo.com>
2018-07-06 15:25:01 +0200

    Add colorMapper util.

---
+++ packages/components/src/__tests__/utils.color.test.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import { colorMapper } from "../utils/color"
+
+describe("ColorMapper", () => {
+  it("should deterministically assign colors to strings", () => {
+    const colors = ["#fff", "#000", "#ccc", "#333", "#666", "#999"]
+    const mapToColor = colorMapper(colors)
+    expect(mapToColor("Tejas")).toEqual("#666")
+    expect(mapToColor("Imogen")).toEqual("#666")
+    expect(mapToColor("Peter")).toEqual("#999")
+    expect(mapToColor("Fabien")).toEqual("#666")
+    expect(mapToColor("Kasi")).toEqual("#333")
+    expect(mapToColor("Michael")).toEqual("#ccc")
+    expect(mapToColor("Tejas")).toEqual("#666")
+    expect(mapToColor("Ian")).toEqual("#000")
+    expect(mapToColor("Chris")).toEqual("#ccc")
+    expect(mapToColor("Lucia")).toEqual("#333")
+    expect(mapToColor("Nick")).toEqual("#ccc")
+    expect(mapToColor("Nic")).toEqual("#333")
+  })
+})
