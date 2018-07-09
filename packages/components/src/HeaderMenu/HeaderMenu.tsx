dee05950a10fdc3ff69a8e673e0276e868e35667
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2018-07-09 10:35:56 +0200

    Improve HeaderMenu alignment.

--- packages/components/src/HeaderMenu/HeaderMenu.tsx
+++ packages/components/src/HeaderMenu/HeaderMenu.tsx
@@ -15,9 +15,12 @@ export interface Props {
 
   /** Display carat on right */
   carat?: boolean
+
+  /** Alignment */
+  align: "left" | "right"
 }
 
-const Container = styled("div")(({ theme }: { theme?: OperationalStyleConstants }) => ({
+const Container = styled("div")(({ theme, align }: { theme?: OperationalStyleConstants; align: "left" | "right" }) => ({
   width: 250,
   lineHeight: "50px",
   padding: `0 ${theme.space.content}px`,
@@ -25,6 +28,12 @@ const Container = styled("div")(({ theme }: { theme?: OperationalStyleConstants 
   backgroundColor: "transparent",
   boxShadow: "none",
   fontWeight: theme.font.weight.medium,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: align === "left" ? "flex-start" : "flex-end",
+  "&>div": {
+    marginLeft: theme.space.small,
+  },
   "&:hover, &.open": {
     color: theme.color.white,
     backgroundColor: "hsla(0, 0%, 100%, 0.1)",
@@ -52,20 +61,23 @@ const ContainerWithCarat = styled(Container)(({ theme }: { theme?: OperationalSt
   },
 }))
 
-const StyledContextMenuItem = styled(ContextMenuItem)(({ theme }: { theme?: OperationalStyleConstants }) => ({
-  color: theme.color.text.default,
-}))
+const StyledContextMenuItem = styled(ContextMenuItem)(
+  ({ theme, align }: { theme?: OperationalStyleConstants; align: "left" | "right" }) => ({
+    color: theme.color.text.default,
+    textAlign: align,
+  }),
+)
 
 const HeaderMenu: React.SFC<Props> = props => {
   const ContainerComponent: any = props.carat ? ContainerWithCarat : Container
   return (
     <ContextMenu noOffset>
-      <ContainerComponent>{props.children}</ContainerComponent>
+      <ContainerComponent align={props.align}>{props.children}</ContainerComponent>
       {props.items.map((option: any) => {
         const onClick = option.onClick || (props.onClick && (() => props.onClick(option)))
         return (
           (option.label || option.value) && (
-            <StyledContextMenuItem onClick={onClick} key={""}>
+            <StyledContextMenuItem onClick={onClick} key={""} align={props.align}>
               {option.label || option.value}
             </StyledContextMenuItem>
           )
@@ -76,6 +88,7 @@ const HeaderMenu: React.SFC<Props> = props => {
 }
 
 HeaderMenu.defaultProps = {
+  align: "left",
   carat: false,
 }
 
