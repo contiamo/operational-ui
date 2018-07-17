ae28040a08a252896818b48027e1dac066afd55b
Fabien BERNARD <fabien0102@hotmail.com>
2018-07-17 10:36:42 +0200

    Remove css props to `withLabel`
    I have some type issue locally with this component when I'm linking

--- packages/components/src/utils/with-label.tsx
+++ packages/components/src/utils/with-label.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import styled from "react-emotion"
 import { OperationalStyleConstants } from "./constants"
-import { Css } from "../types"
 
 const Container = styled("div")(({ theme }: { theme?: OperationalStyleConstants }) => ({
   display: "inline-block",
@@ -15,16 +14,14 @@ const Label = styled("label")(({ theme }: { theme?: OperationalStyleConstants })
   marginBottom: theme.deprecated.spacing / 4,
 }))
 
-type IComponent = Pick<React.HTMLProps<HTMLDivElement>, "className" | "label" | "id"> & {
-  css?: Css
-}
+type IComponent = Pick<React.HTMLProps<HTMLDivElement>, "className" | "label" | "id">
 
 function withLabel<T extends IComponent>(Component: React.ComponentType<T>): React.SFC<T> {
-  return (props: T & IComponent) => {
+  return (props: T) => {
     const { id, label } = props
     const domId = id || (label && label.toLowerCase ? label.toLowerCase().replace(/\s/g, "-") : null)
     return (
-      <Container className={props.className} css={props.css}>
+      <Container className={props.className}>
         {props.label && <Label htmlFor={domId}>{props.label}</Label>}
         {props.label && <br />}
         <Component {...props} id={domId} />
