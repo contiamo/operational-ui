b7ba349b2c4802456bd0df52cc9ef2335a61656e
peterszerzo <szerzo.peter@gmail.com>
2018-07-17 11:15:14 +0200

    Remove visualizations package and related scripts

--- packages/visualizations/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Simple imports and exports for consumers of the library.
-export { default as Chart } from "./Chart/facade"
-export { AxesData } from "./Chart/typings"
-export { default as PieChart } from "./PieChart/facade"
-export { default as ProcessFlow } from "./ProcessFlow/facade"
-export { default as ProcessFlowLoopHandler } from "./utils/process_flow_loop_handler"
-export { default as Sunburst } from "./Sunburst/facade"
-export { default as VisualizationWrapper } from "./utils/VisualizationWrapper"
--- packages/visualizations/src/utils/__tests__/font_sizing_utils.test.ts
+++
@@ -1,9 +0,0 @@
-import { approxZero } from "../font_sizing_utils"
-
-describe("font sizing utils", () => {
-  it("approxZero", () => {
-    const upperLimit: number = 51
-    const stepFunction = (x: number) => upperLimit - x
-    expect(approxZero(stepFunction, 100)).toEqual(50)
-  })
-})
