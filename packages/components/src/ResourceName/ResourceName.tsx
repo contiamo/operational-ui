fd31b6a56d36fe5c4bae234ff29c51ee7dba8405
Fabien BERNARD <fabien0102@hotmail.com>
2018-07-20 14:55:43 +0200

    Fix ResourceName…

--- packages/components/src/ResourceName/ResourceName.tsx
+++ packages/components/src/ResourceName/ResourceName.tsx
@@ -2,8 +2,8 @@ import styled from "react-emotion"
 import { OperationalStyleConstants } from "../utils/constants"
 
 export const ResourceName = styled("span")`
-  font-family: ${({ theme }: { theme: OperationalStyleConstants }) => theme.font.family.code};
-  color: ${({ theme }: { theme: OperationalStyleConstants }) => theme.color.text.dark};
+  font-family: ${({ theme }: { theme?: OperationalStyleConstants }) => theme.font.family.code};
+  color: ${({ theme }: { theme?: OperationalStyleConstants }) => theme.color.text.dark};
 `
 
 export default ResourceName
