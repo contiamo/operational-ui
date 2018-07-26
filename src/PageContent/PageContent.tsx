192919882befa20d325dd8fd2cdc1505467deb9b
Fabien BERNARD <fabien0102@hotmail.com>
2018-07-26 15:18:33 +0200

    Add some sugar to PageContent (no need to PageArea if you have only one element)

--- src/PageContent/PageContent.tsx
+++ src/PageContent/PageContent.tsx
@@ -1,7 +1,10 @@
 import * as React from "react"
+import PageArea, { PageAreaProps } from "../PageArea/PageArea"
 import styled from "../utils/styled"
 
 export interface Props {
+  /** Children to render, you */
+  children?: PageAreaProps["children"]
   /** Areas template for `PageArea` disposition */
   areas?: "main" | "main side" | "side main"
   /** Fill the entire width */
@@ -30,6 +33,20 @@ const StyledPageContent = styled("div")<{ areas?: Props["areas"]; isFill?: boole
 })
 
 // `fill` must be rename internally to avoid conflict with the native `fill` DOM attribute
-const PageContent: React.SFC<Props> = ({ fill, ...props }) => <StyledPageContent {...props} isFill={fill} />
+const PageContent: React.SFC<Props> = ({ fill, children, ...props }) => {
+  const oneChild = typeof children === "function" || React.Children.count(children) === 1
+
+  return (
+    <StyledPageContent {...props} isFill={fill}>
+      {oneChild ? (
+        <PageArea>
+          {modalConfirmContext => (typeof children === "function" ? children(modalConfirmContext) : children)}
+        </PageArea>
+      ) : (
+        children
+      )}
+    </StyledPageContent>
+  )
+}
 
 export default PageContent
