fb6a0bec68ad816ef1fc66ab33a747add0f98ebc
Fabien BERNARD <fabien0102@hotmail.com>
2018-08-03 14:24:53 +0200

    Adjust Fineprint margin

--- src/Typography/FinePrint.tsx
+++ src/Typography/FinePrint.tsx
@@ -1,12 +1,15 @@
 import styled from "../utils/styled"
 
 export const FinePrint = styled("p")(({ theme }) => ({
-  fontSize: theme.font.size.small,
+  fontSize: theme.font.size.fineprint,
   fontFamily: theme.font.family.main,
   fontWeight: theme.font.weight.regular,
   lineHeight: theme.font.lineHeight,
-  margin: `${theme.space.base}px 0`,
+  margin: `${theme.space.small}px 0 ${theme.space.big}px 0`,
   color: theme.color.text.lightest,
+  ":last-child": {
+    marginBottom: 0,
+  },
 }))
 
 export default FinePrint
