0ac23f755d023ed70348944ce21de77a525e354c
Fabien BERNARD <fabien0102@hotmail.com>
2018-08-06 17:39:58 +0200

    Add DeepPartial util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,3 +22,12 @@ export const getInitials = (name: string): string => {
   const [firstInitial, , lastInitial] = fullInitials
   return fullInitials.length > 2 ? firstInitial + lastInitial : fullInitials
 }
+
+/**
+ * Make a type and all its (sub) properties optional.
+ */
+export type DeepPartial<T> = T extends any[] ? DeepPartialArray<T[number]> : T extends object ? DeepPartialObject<T> : T
+
+interface DeepPartialArray<T> extends Array<DeepPartial<T>> {}
+
+type DeepPartialObject<T> = { [P in keyof T]+?: DeepPartial<T[P]> }
