2bd379e84f9b5619227b41a19c38bc110780f36f
Eric Adamski <er.adamski@gmail.com>
2018-09-06 07:11:16 -0400

    Remove the card styles intended to help the modal
    Added a new wrapper styled component that controls the content of the modal

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -117,10 +117,6 @@ const Container = styled("div")(({ theme }) => ({
 const Content = styled("div")`
   ${({ theme }) => `
     padding: ${theme.space.element}px;
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
   `};
 `
 
