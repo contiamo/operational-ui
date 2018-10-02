1a8447ef633913f04b78f66046cbf979b77f163c
Tejas Kumar <tejas@contiamo.com>
2018-10-02 19:36:07 +0200

    Add keyboard functionality for a11y

--- src/ContextMenu/ContextMenu.tsx
+++ src/ContextMenu/ContextMenu.tsx
@@ -1,4 +1,6 @@
+import { isEqual } from "lodash"
 import * as React from "react"
+
 import { DefaultProps } from "../types"
 import styled from "../utils/styled"
 
@@ -33,6 +35,7 @@ export interface ContextMenuProps extends DefaultProps {
 
 export interface State {
   isOpen: boolean
+  focusedItemIndex: number
 }
 
 const Container = styled("div")(({ align }: { align: ContextMenuProps["align"] }) => ({
@@ -61,9 +64,19 @@ const MenuContainer = styled("div")<{
   display: isExpanded ? "block" : "none",
 }))
 
+const keyCodes = {
+  up: 38,
+  down: 40,
+  enter: 13,
+  esc: 27,
+}
+
 class ContextMenu extends React.Component<ContextMenuProps, State> {
-  public state = {
+  public menu: HTMLDivElement | null = null
+
+  public state: State = {
     isOpen: false,
+    focusedItemIndex: 0,
   }
 
   public static defaultProps: Partial<ContextMenuProps> = {
@@ -71,11 +84,17 @@ class ContextMenu extends React.Component<ContextMenuProps, State> {
     embedChildrenInMenu: false,
   }
 
-  public componentDidUpdate() {
+  public componentDidUpdate(prevProps: ContextMenuProps) {
     if (this.state.isOpen) {
       document.addEventListener("click", this.toggle)
     } else {
       document.removeEventListener("click", this.toggle)
+    }
+
+    // Reset focused item to first if items change.
+    if (!isEqual(this.props.items, prevProps.items)) {
+      this.setState(() => ({ focusedItemIndex: 0 }))
+      this.focusElement()
     }
   }
 
@@ -84,6 +103,37 @@ class ContextMenu extends React.Component<ContextMenuProps, State> {
       isOpen: !prevState.isOpen,
     }))
 
+  private focusElement = () => {
+    if (this.menu && this.menu.querySelector('[tabindex="0"]')) {
+      setTimeout(() => (this.menu!.querySelector('[tabindex="0"]') as HTMLDivElement).focus())
+    }
+  }
+
+  private onUpPress = () => ({
+    focusedItemIndex: this.state.focusedItemIndex === 0 ? this.props.items.length - 1 : this.state.focusedItemIndex - 1,
+  })
+
+  private onDownPress = () => ({
+    focusedItemIndex: this.state.focusedItemIndex === this.props.items.length - 1 ? 0 : this.state.focusedItemIndex + 1,
+  })
+
+  private handleKeyPress = ({ keyCode }: React.KeyboardEvent<HTMLDivElement>) => {
+    if (keyCode === keyCodes.enter && this.props.onClick) {
+      this.props.onClick(this.props.items[this.state.focusedItemIndex])
+      this.setState(() => ({ isOpen: false }))
+      return
+    }
+
+    if (keyCode === keyCodes.esc) {
+      this.setState(() => ({ isOpen: false }))
+    }
+
+    if ([keyCodes.up, keyCodes.down].includes(keyCode)) {
+      this.setState(keyCode === keyCodes.up ? this.onUpPress : this.onDownPress)
+      this.focusElement()
+    }
+  }
+
   public render() {
     if (!this.props.items) {
       throw new Error("No array of items has been provided for the ContextMenu.")
@@ -91,30 +141,34 @@ class ContextMenu extends React.Component<ContextMenuProps, State> {
 
     const { items, condensed, iconLocation, children, open, embedChildrenInMenu, align, width, ...props } = this.props
 
-    const renderedChildren =
-      typeof children === "function"
-        ? (children as ((isActive: boolean) => React.ReactNode))(this.state.isOpen)
-        : children
+    const renderedChildren = typeof children === "function" ? children(this.state.isOpen) : children
     return (
-      <Container {...props} align={align} onClick={this.toggle}>
+      <Container {...props} align={align} onClick={this.toggle} onKeyUp={this.handleKeyPress}>
         {renderedChildren}
-        <MenuContainer isExpanded={open || this.state.isOpen} embedChildrenInMenu={this.props.embedChildrenInMenu}>
-          {embedChildrenInMenu && renderedChildren}
-          {items.map((item: string | IContextMenuItem, index: number) => {
-            const clickHandler = (typeof item !== "string" && item.onClick) || this.props.onClick
-            return (
-              <ContextMenuItem
-                onClick={clickHandler && (() => clickHandler(item))}
-                key={`contextmenu-${index}`}
-                condensed={condensed}
-                align={align}
-                iconLocation={iconLocation}
-                width={width || "100%"}
-                item={item}
-              />
-            )
-          })}
-        </MenuContainer>
+        {this.state.isOpen && (
+          <MenuContainer
+            innerRef={node => (this.menu = node)}
+            isExpanded={open || this.state.isOpen}
+            embedChildrenInMenu={this.props.embedChildrenInMenu}
+          >
+            {embedChildrenInMenu && renderedChildren}
+            {items.map((item: string | IContextMenuItem, index: number) => {
+              const clickHandler = (typeof item !== "string" && item.onClick) || this.props.onClick
+              return (
+                <ContextMenuItem
+                  tabIndex={this.state.focusedItemIndex === index ? 0 : -1} // ref "tabindex roving": https://developers.google.com/web/fundamentals/accessibility/focus/using-tabindex
+                  onClick={clickHandler && (() => clickHandler(item))}
+                  key={`contextmenu-${index}`}
+                  condensed={condensed}
+                  align={align}
+                  iconLocation={iconLocation}
+                  width={width || "100%"}
+                  item={item}
+                />
+              )
+            })}
+          </MenuContainer>
+        )}
       </Container>
     )
   }
