81105f1d7d091363ed2345cd034c796022ff860a
Tejas Kumar <tejas@contiamo.com>
2019-02-14 19:06:31 +0100

    Add useUniqueId hook

---
+++ src/hooks/useUniqueId/index.ts
@@ -0,0 +1,11 @@
+import nanoid from "nanoid"
+import { useRef } from "react"
+
+export const useUniqueId = (defaultId: string | null = null) => {
+  const id = useRef(defaultId)
+  if (!id.current) {
+    // @ts-ignore this is not read-only
+    id.current = nanoid()
+  }
+  return id.current
+}
