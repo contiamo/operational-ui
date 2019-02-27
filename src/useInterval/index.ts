2f0a4af970e55a20181359ee4b9555a8d09114d5
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-02-27 17:09:20 -0500

    Add useInterval

---
+++ src/useInterval/index.ts
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react"
+
+// Ref: https://github.com/Hermanya/use-interval/blob/master/src/index.tsx
+
+// tslint:disable-next-line
+const noop = () => {}
+
+export const useInterval = (callback: () => void, delay: number | null, immediate?: boolean) => {
+  const savedCallback = useRef(noop)
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback
+  })
+
+  // Execute callback if immediate is set.
+  useEffect(() => {
+    if (immediate && delay !== null) {
+      savedCallback.current()
+    }
+  }, [immediate])
+
+  // Set up the interval.
+  useEffect(() => {
+    if (delay === null) {
+      return undefined
+    }
+    const tick = () => savedCallback.current()
+    const id = setInterval(tick, delay)
+    return () => clearInterval(id)
+  }, [delay])
+}
+
+export default useInterval
