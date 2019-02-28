0b068bda647b416163001759cb705a4a4f446148
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-02-28 15:23:16 -0500

    useInterval updates

--- src/useInterval/index.ts
+++ src/useInterval/index.ts
@@ -1,9 +1,7 @@
+import { noop } from "lodash"
 import { useEffect, useRef } from "react"
 
-// Ref: https://github.com/Hermanya/use-interval/blob/master/src/index.tsx
-
-// tslint:disable-next-line
-const noop = () => {}
+// From: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 
 /**
  * Hook version of setInterval. Pass null to the delay to cancel an execution.
@@ -13,28 +11,29 @@ export function useInterval(callback: () => void, delay: number | null, immediat
 
   // Remember the latest callback.
   // useEffect has no second argument so it will be executed after each render
-  // but we don't want to change this value directly in the body of the render function, 
+  // but we don't want to change this value directly in the body of the render function,
   // because render should be pure function
+
+  // After every render, save the latest callback into our ref.
   useEffect(() => {
     savedCallback.current = callback
   })
 
-  // Execute callback if immediate is set.
   useEffect(() => {
     if (immediate && delay !== null) {
       savedCallback.current()
     }
-  }, [immediate])
+  }, [immediate]) // when immediate changes, we want to restart the timer
 
   // Set up the interval.
   useEffect(() => {
     if (delay === null) {
       return undefined
     }
-    const tick = () => savedCallback.current()
-  const id = setInterval(() => savedCallback.current(), delay)
+    // we can read and call latest callback from inside our interval:
+    const id = setInterval(() => savedCallback.current(), delay)
     return () => clearInterval(id)
-  }, [delay])
+  }, [delay]) // when delay changes, we want to restart the timer
 }
 
 export default useInterval
