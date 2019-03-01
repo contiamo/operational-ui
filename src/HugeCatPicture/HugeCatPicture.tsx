f39c18ea5f232261f334ecb21fbe949f2d11a6e3
Mykhailo Potomin <mpotomin@Mykhailos-MacBook-Pro.local>
2019-03-01 15:13:50 +0100

    Add test cat

---
+++ src/HugeCatPicture/HugeCatPicture.tsx
@@ -0,0 +1,55 @@
+import { keyframes } from "@emotion/core"
+import * as React from "react"
+import { expandColor } from "../utils/constants"
+import styled from "../utils/styled"
+
+export interface HugeCatPictureProps {
+  size: number
+  borderColor?: string
+  spin?: boolean
+}
+
+const spinAnimation = keyframes`
+  0% {
+    transform: rotate(0deg)
+  }
+
+  100% {
+    transform: rotate(360deg)
+  }
+`
+
+const Container = styled("div")<HugeCatPictureProps>`
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
+  border: 4px solid ${({ borderColor, theme }) => (borderColor ? expandColor(theme, borderColor) : "black")};
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${({ spin }) => (spin ? `${spinAnimation.name} 3s infinite linear;` : "")};
+  > img {
+    height: 100%;
+  }
+
+  ${spinAnimation.styles}
+`
+
+export const HugeCatPicture: React.FC<HugeCatPictureProps> = props => {
+  const [photoUrl, setPhotoUrl] = React.useState("")
+
+  // console.log(`spin:${props.spin}`)
+
+  React.useEffect(() => {
+    fetch("https://api.thecatapi.com/v1/images/search?limit=1&order=Random&size=small&page=1&sub_id=demo-bc30a9")
+      .then(response => response.json())
+      .then(resultArray => {
+        setPhotoUrl(resultArray[0].url)
+      })
+  }, [])
+
+  return <Container {...props}>{Boolean(photoUrl) && <img alt="Cat" src={photoUrl} />}</Container>
+}
+
+export default HugeCatPicture
---
+++ src/HugeCatPicture/README.md
@@ -0,0 +1,16 @@
+# Basic Usage
+
+Use this component like so
+
+```jsx
+<div style={{ display: "flex" }}>
+  <HugeCatPicture size={400} borderColor="#f00" />
+  <HugeCatPicture size={400} borderColor="primary" />
+  <HugeCatPicture size={400} />
+  </div>
+<div style={{ position: "absolute", top: 175, left: 150, display: "flex" }}>
+
+  <HugeCatPicture size={400} borderColor="color.background.lightest" />
+  <HugeCatPicture size={400} spin />
+</div>
+```
