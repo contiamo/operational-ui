4fafdb847f0339317501cac20efdaaab36efb7fc
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-03-01 23:25:44 -0500

    Remove consumer - may need to find solution for activeComp

--- styleguide/ComponentsListRenderer.tsx
+++ styleguide/ComponentsListRenderer.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 
 import { SidenavHeader, SidenavItem } from "../src"
-import { Consumer } from "./StyleGuideRenderer"
 
 export interface ComponentsListItem {
   heading: string
@@ -13,23 +12,18 @@ export interface ComponentsListRendererProps {
   items: ComponentsListItem[]
 }
 
-const ComponentsListRenderer: React.SFC<ComponentsListRendererProps> = ({ items }) => (
-  <Consumer>
-    {({ activeComponent }) =>
-      items.map(({ name, components }) => (
-        <SidenavHeader active={true} label={name} key={name}>
-          {components.map(component => (
-            <SidenavItem
-              active={activeComponent === component.name}
-              key={`${name}-${component.name}`}
-              label={component.name}
-              to={`#${component.name}`}
-            />
-          ))}
-        </SidenavHeader>
-      ))
-    }
-  </Consumer>
-)
+const ComponentsListRenderer = ({ items }: ComponentsListRendererProps) =>
+  items.map(({ name, components }) => (
+    <SidenavHeader active={true} label={name} key={name}>
+      {components.map(component => (
+        <SidenavItem
+          active={window.location.hash === `#/${component.name}`}
+          key={`${name}-${component.name}`}
+          label={component.name}
+          to={`#/${component.name}`}
+        />
+      ))}
+    </SidenavHeader>
+  ))
 
 export default ComponentsListRenderer
