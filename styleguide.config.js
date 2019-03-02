51e5ec8c074dff3ce687fa16d3fde628dacab13f
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-03-01 23:27:06 -0500

    Clean up config- add pagepersection

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,24 +1,16 @@
 // styleguide.config.js
 const { join, resolve } = require("path")
-const reactDocgenTypescript = require("react-docgen-typescript")
+const { parse: propsParser } = require("react-docgen-typescript")
 
 module.exports = {
   title: "Operational UI",
-
-  propsParser: reactDocgenTypescript.withDefaultConfig({
-    propFilter(prop) {
-      if (prop.parent) {
-        return !prop.parent.fileName.includes("node_modules")
-      }
-
-      return true
-    },
-  }).parse,
+  propsParser,
   pagePerSection: true,
   moduleAliases: {
     "@operational/components": resolve(__dirname, "src"),
   },
   sections: [
+    /// Should add an Intro Page to be first loaded after splash
     { name: "Hooks", components: "src/use*/*.ts" },
     {
       name: "Components",
@@ -34,20 +26,7 @@ module.exports = {
       ],
       sectionDepth: 1,
     },
-    // {
-    //   name: "Components",
-    //   components: ["src/Card/*.tsx","src/Button/*.tsx"],
-    //   ignore: [
-    //     "/**/*test*",
-    //     "**/Internals/**",
-    //     "**/hooks/**",
-    //     "**/utils/**",
-    //     "**/Typography/**",
-    //     "**/Debug/makeRows*",
-    //     "**/*.*.{ts,tsx}",
-    //   ],
-    //   sectionDepth: 1,
-    // },
+
     { name: "Typography", components: "src/Typography/*.tsx", sectionDepth: 1 },
   ],
   theme: {
@@ -57,13 +36,13 @@ module.exports = {
   },
   styleguideComponents: {
     Wrapper: join(__dirname, "styleguide/Wrapper"),
-    // StyleGuideRenderer: join(__dirname, "styleguide/StyleGuideRenderer"),
-    // TableOfContentsRenderer: join(__dirname, "styleguide/TableOfContentsRenderer"),
-    // ComponentsListRenderer: join(__dirname, "styleguide/ComponentsListRenderer"),
-    // ReactComponentRenderer: join(__dirname, "styleguide/ReactComponentRenderer"),
-    // SectionHeadingRenderer: join(__dirname, "styleguide/SectionHeadingRenderer"),
-    //  HeadingRenderer: join(__dirname, "styleguide/HeadingRenderer"),
-    // TabButtonRenderer: join(__dirname, "styleguide/TabButtonRenderer"),
+    StyleGuideRenderer: join(__dirname, "styleguide/StyleGuideRenderer"),
+    TableOfContentsRenderer: join(__dirname, "styleguide/TableOfContentsRenderer"),
+    ComponentsListRenderer: join(__dirname, "styleguide/ComponentsListRenderer"),
+    ReactComponentRenderer: join(__dirname, "styleguide/ReactComponentRenderer"),
+    SectionHeadingRenderer: join(__dirname, "styleguide/SectionHeadingRenderer"),
+    HeadingRenderer: join(__dirname, "styleguide/HeadingRenderer"),
+    TabButtonRenderer: join(__dirname, "styleguide/TabButtonRenderer"),
   },
   context: {
     styled: "@emotion/styled",
