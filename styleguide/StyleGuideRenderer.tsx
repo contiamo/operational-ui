cd4e9541b5da9a8deadb59126d218697a2aceb31
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-03-04 18:38:41 -0500

    Version is passed through config, make replace class with func

--- styleguide/StyleGuideRenderer.tsx
+++ styleguide/StyleGuideRenderer.tsx
@@ -1,18 +1,15 @@
 import { css, Global } from "@emotion/core"
-import * as React from "react"
+import React, { useState } from "react"
 import styled from "../src/utils/styled"
 
 import { Button, HeaderBar, Layout, Logo, OperationalUI, Page, Splash } from "../src"
 import constants from "../src/utils/constants"
 
-export interface StyleGuideRendererState {
-  isSplashVisible: boolean
-}
-
 export interface StyleGuideRendererProps {
   children: React.ReactNode[] | React.ReactNode
   toc: React.ReactNode
   hasSidebar: boolean
+  version: string
 }
 
 /**
@@ -24,7 +21,6 @@ export interface StyleGuideRendererProps {
  * for the version value.
  */
 /* tslint:disable:no-var-requires */
-const { version } = require("../package.json")
 
 const Version = styled("div")`
   font-size: 16px;
@@ -32,81 +28,73 @@ const Version = styled("div")`
   margin-right: ${constants.space.content}px;
 `
 
-const Header = () => <HeaderBar logo={<Logo name="OperationalUI" />} end={<Version>v{version}</Version>} />
+const Header = ({ version }: { version: string }) => (
+  <HeaderBar logo={<Logo name="OperationalUI" />} end={<Version>v{version}</Version>} />
+)
 
-class StyleGuideRenderer extends React.Component<StyleGuideRendererProps, Readonly<StyleGuideRendererState>> {
-  public readonly state = {
-    isSplashVisible: !Boolean(window.location.hash),
-  }
+const SplashScreen = ({ setSplash }: { setSplash: React.Dispatch<React.SetStateAction<boolean>> }) => (
+  <Splash
+    color="#005f96"
+    title="Operational UI"
+    actions={
+      <>
+        <Button onClick={() => setSplash(false)}>Docs</Button>
+        <Button to="https://github.com/contiamo/operational-ui/">GitHub</Button>
+      </>
+    }
+  >
+    <p>
+      Operational is a UI library optimized for day-to-day operational decision-making. It does its best when used for
+      interfaces that assume familiarity through routine use, prioritizing compactness and{" "}
+      {
+        <a href="https://twitter.com/edwardtufte/status/450076034759524352" target="_blank">
+          small effective differences
+        </a>
+      }
+      .
+    </p>
+    <p>It is predictable to use, and it lets you and your team breathe. Exhales, not sighs.</p>
+  </Splash>
+)
 
-  public render() {
-    const { children, toc, hasSidebar } = this.props
-    const { isSplashVisible } = this.state
-    return (
-      <OperationalUI>
-        <Global
-          styles={css`
-            #rsg-root {
-              height: 100vh;
-            }
+function StyleGuideRenderer({ children, toc, hasSidebar, version }: StyleGuideRendererProps) {
+  const [isSplashVisible, setSplash] = useState(() => !Boolean(window.location.hash))
 
-            // buttons style override
-            [class^="rsg--controls-"] {
-              margin-top: 16px;
-            }
+  return (
+    <OperationalUI>
+      <Global
+        styles={css`
+          #rsg-root {
+            height: 100vh;
+          }
 
-            .rsg--spacing-7 {
-              margin: 0;
-            }
+          // buttons style override
+          [class^="rsg--controls-"] {
+            margin-top: 16px;
+          }
 
-            .rsg--tab-14: {
-              margintop: constants.space.content;
-            }
-          `}
-        />
+          .rsg--spacing-7 {
+            margin: 0;
+          }
 
-        {isSplashVisible ? (
-          <Splash
-            color="#005f96"
-            title="Operational UI"
-            actions={
-              <>
-                <Button
-                  onClick={() => {
-                    this.setState(() => ({
-                      isSplashVisible: false,
-                    }))
-                  }}
-                >
-                  Docs
-                </Button>
-                <Button to="https://github.com/contiamo/operational-ui/">GitHub</Button>
-              </>
-            }
-          >
-            <p>
-              Operational is a UI library optimized for day-to-day operational decision-making. It does its best when
-              used for interfaces that assume familiarity through routine use, prioritizing compactness and{" "}
-              {
-                <a href="https://twitter.com/edwardtufte/status/450076034759524352" target="_blank">
-                  small effective differences
-                </a>
-              }
-              .
-            </p>
-            <p>It is predictable to use, and it lets you and your team breathe. Exhales, not sighs.</p>
-          </Splash>
-        ) : hasSidebar ? (
-          <Layout header={<Header />} sidenav={toc} main={<Page title="Components">{children}</Page>} />
-        ) : (
-          <>
-            <Header />
-            <Page fill>{children}</Page>
-          </>
-        )}
-      </OperationalUI>
-    )
-  }
+          .rsg--tab-14: {
+            margintop: constants.space.content;
+          }
+        `}
+      />
+
+      {isSplashVisible ? (
+        <SplashScreen setSplash={setSplash} />
+      ) : hasSidebar ? (
+        <Layout header={<Header version={version} />} sidenav={toc} main={<Page title="Components">{children}</Page>} />
+      ) : (
+        <>
+          <Header version={version} />
+          <Page fill>{children}</Page>
+        </>
+      )}
+    </OperationalUI>
+  )
 }
 
 export default StyleGuideRenderer
