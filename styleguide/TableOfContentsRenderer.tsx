3dcf1d5a31234e5d48a23c240813f44d28fc6bc5
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-03-04 19:11:18 -0500

    ** Fix Search Input

--- styleguide/TableOfContentsRenderer.tsx
+++ styleguide/TableOfContentsRenderer.tsx
@@ -1,5 +1,5 @@
-import * as React from "react"
-import { Input, Sidenav, SidenavHeader } from "../src"
+import React from "react"
+import { Input, Sidenav, styled } from "../src"
 
 export interface TableOfContentsRendererProps {
   children: React.ReactElement<any>
@@ -7,14 +7,41 @@ export interface TableOfContentsRendererProps {
   onSearchTermChange: (newVal: string) => void
 }
 
+// Dont pass Input in RenderProps-(<SideNavHeader label={<Input/>} />) makes Compnent rerender and loses input focus
+// Temporarily- manually configure like a SideNavHeader
+// Another example why All presentational components should also be exported.
+
+const Content = styled("div")(({ theme }) => ({
+  textDecoration: "none",
+  cursor: "pointer",
+  position: "relative",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  justifyContent: "center",
+  height: 73,
+  overflow: "hidden",
+  padding: `0 ${theme.space.content}px`,
+  width: "100%",
+  marginBottom: 0,
+}))
+
+const Container = styled("div")(({ theme }) => ({
+  label: "sidenavheader",
+  textDecoration: "none",
+  width: "100%",
+  borderBottom: "1px solid",
+  borderBottomColor: theme.color.separators.default,
+}))
+
 const TableOfContentsRenderer: React.SFC<TableOfContentsRendererProps> = ({
   children,
   searchTerm,
   onSearchTermChange,
 }) => (
   <Sidenav>
-    <SidenavHeader
-      label={
+    <Container>
+      <Content>
         <Input
           fullWidth
           value={searchTerm}
@@ -22,8 +49,8 @@ const TableOfContentsRenderer: React.SFC<TableOfContentsRendererProps> = ({
           aria-label="Search Components..."
           onChange={onSearchTermChange}
         />
-      }
-    />
+      </Content>
+    </Container>
     {children}
   </Sidenav>
 )
