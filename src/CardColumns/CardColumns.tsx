bf0add28736408d33e754cd942e1ce8c4f4add45
stereobooster <stereobooster@gmail.com>
2019-03-05 16:04:38 +0100

    Add columns param to CardColumns

--- src/CardColumns/CardColumns.tsx
+++ src/CardColumns/CardColumns.tsx
@@ -1,13 +1,18 @@
 import * as React from "react"
 import styled from "../utils/styled"
 
-export const CardColumns = styled("div")<React.Props<{}>>(({ children, theme }) => ({
-  display: "flex",
-  flexWrap: "wrap",
-  margin: -(theme.space.element / 2),
-  "& > *": {
-    flexBasis: `${React.Children.count(children)}%`,
-  },
-}))
+export const CardColumns = styled("div")<React.Props<{}> & { columns?: number }>(({ children, theme, columns }) => {
+  columns = columns === undefined ? React.Children.count(children) : columns
+  return {
+    display: "flex",
+    flexWrap: "wrap",
+    margin: -(theme.space.element / 2),
+    "& > *": {
+      // !important required for flexColumn
+      flexBasis: `calc(100% / ${columns}) !important`,
+      maxWidth: `calc(100% / ${columns})`,
+    },
+  }
+})
 
 export default CardColumns
--- src/CardColumns/README.md
+++ src/CardColumns/README.md
@@ -126,3 +126,23 @@ The `CardColumns` component is used as a wrapper around groups of `CardColumn` c
   </CardColumns>
 </Card>
 ```
+
+### With columns param
+
+```jsx
+<Card title="Bundle information">
+  <p>Here is the information available about this bundle.</p>
+  <CardColumns columns={3}>
+    <CardColumn title="Contributors">
+      <AvatarGroup>
+        <Avatar name="Alice Bernoulli" />
+        <Avatar name="Clarence Dermot" />
+      </AvatarGroup>
+    </CardColumn>
+    <CardColumn title="Tags">
+      <Chip>agent-view</Chip>
+      <Chip>production</Chip>
+    </CardColumn>
+  </CardColumns>
+</Card>
+```
