e142dd837cc6c970d89aff5478fddda67b66b5da
Tejas Kumar <tejas@contiamo.com>
2019-04-09 11:13:53 +0200

    [Table] Remove hover if no click action

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -229,7 +229,7 @@ function Table<T>({
             })()
             return (
               <Tr
-                hover={Boolean(data)}
+                hover={Boolean(onRowClick)}
                 key={dataEntryIndex}
                 clickable={Boolean(onRowClick)}
                 onClick={() => {
