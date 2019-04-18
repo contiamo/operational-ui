1d07176928c85335b03905fb68fb244e0ea21550
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-04-18 19:36:51 -0400

    add Button test

--- src/Button/__tests__/Button.test.tsx
+++ src/Button/__tests__/Button.test.tsx
@@ -1,5 +1,6 @@
 import { render } from "enzyme"
 import * as React from "react"
+import { itShouldOnlyMountOnce } from "../../__testUtils__/itShouldOnlyMountOnce"
 import { Button as ThemelessButton } from "../../index"
 import wrapDefaultTheme from "../../utils/wrap-default-theme"
 
@@ -9,4 +10,6 @@ describe("Button Component", () => {
   it("Should initialize properly", () => {
     expect(render(<Button>hi</Button>)).toMatchSnapshot()
   })
+
+  itShouldOnlyMountOnce("Button", Button)
 })
