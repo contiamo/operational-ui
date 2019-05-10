4d5afcf42cbd712a4308bcdb0d0321af37c4537f
Tejas Kumar <tejas@contiamo.com>
2019-05-10 18:11:53 +0200

    Update DataTable - Scroll while headers stay fixed - Adjust padding/sizing - Adjust Typography

--- src/DataTable/DataTable.tsx
+++ src/DataTable/DataTable.tsx
@@ -2,11 +2,15 @@ import * as React from "react"
 import styled from "../utils/styled"
 
 export interface DataTableProps<T> {
+  /** A collection of rows for the table */
   rows: Array<{
     isHeading?: boolean
     isDisabled?: boolean
     cells: T
   }>
+
+  /** How much shall we restrict the height? */
+  height?: number
 }
 
 const Container = styled("div")`
@@ -14,8 +18,15 @@ const Container = styled("div")`
   box-shadow: ${({ theme }) => "0 0 1px 1px inset " + theme.color.border.invisible};
 `
 
+const TableBody = styled("tbody", { shouldForwardProp: prop => prop !== "height" })<{ height?: number }>`
+  height: ${({ height }) => height + "px" || "auto"};
+  overflow: auto;
+`
+
 const Table = styled("table")`
   border-collapse: collapse;
+  table-layout: fixed;
+  border-spacing: 0;
 `
 
 const Row = styled("tr")<{
@@ -29,20 +40,21 @@ const Row = styled("tr")<{
   ${({ isHeading, theme }) =>
     isHeading
       ? `
-  :first-child {
-    background-color: ${theme.color.background.light};
-    color: ${theme.color.text.light};
-  }
-
-    background-color: ${theme.color.background.lightest};
-    color: ${theme.color.text.light};
+    background-color: ${theme.color.background.gentle};
+    color: ${theme.color.text.dark};
   `
       : ""}
 `
 
-const Cell = styled("td")`
-  border: 1px solid ${({ theme }) => theme.color.border.default};
+const Cell = styled<"td" | "th">("td")<{ as: "td" | "th"; isHeading: boolean }>`
+  border: 1px solid;
+  border-top-color: ${({ theme }) => theme.color.border.medium};
+  border-bottom-color: ${({ theme }) => theme.color.border.medium};
+  border-left-color: ${({ theme }) => theme.color.border.lightest};
+  border-right-color: ${({ theme }) => theme.color.border.lightest};
+  font-family: ${({ theme, isHeading }) => (isHeading ? theme.font.family.main : theme.font.family.code)};
   padding: 0;
+  height: 36px;
 `
 
 const Value = styled("div")`
@@ -50,34 +62,28 @@ const Value = styled("div")`
   align-items: center;
   width: 100%;
   height: 100%;
-  padding: ${({ theme }) => theme.space.medium}px ${({ theme }) => theme.space.content}px;
+  padding: 10px ${({ theme }) => theme.space.content}px;
 `
 
-const EditButton = styled(Cell)`
-  text-align: center;
-  vertical-align: middle;
-  background-color: ${({ theme }) => theme.color.background.lightest};
-  width: 150px;
-  cursor: pointer;
+const dataToRow = ({ isHeading, isDisabled, cells }: DataTableProps<any[]>["rows"][-1], rowIndex: number) => (
+  <Row isDisabled={isDisabled} isHeading={isHeading} key={rowIndex}>
+    {cells.map((cellValue, cellIndex) => (
+      <Cell isHeading={Boolean(isHeading)} as={isHeading ? "th" : "td"} key={rowIndex * cellIndex}>
+        <Value>{cellValue}</Value>
+      </Cell>
+    ))}
+  </Row>
+)
 
-  :hover {
-    background-color: ${({ theme }) => theme.color.background.light};
-  }
-`
+export function DataTable<T extends P[], P = any>({ rows, height }: DataTableProps<T>) {
+  const headers = rows.filter(row => row.isHeading)
+  const regularRows = rows.filter(row => !row.isHeading)
 
-export function DataTable<T extends P[], P = any>({ rows }: DataTableProps<T>) {
   return (
     <Container>
       <Table>
-        {rows.map(({ isHeading, isDisabled, cells }, rowIndex) => (
-          <Row isDisabled={isDisabled} isHeading={isHeading} key={rowIndex}>
-            {cells.map((cellValue, cellIndex) => (
-              <Cell key={rowIndex * cellIndex}>
-                <Value>{cellValue}</Value>
-              </Cell>
-            ))}
-          </Row>
-        ))}
+        <thead>{headers.map(dataToRow)}</thead>
+        <TableBody height={height}>{regularRows.map(dataToRow)}</TableBody>
       </Table>
     </Container>
   )
