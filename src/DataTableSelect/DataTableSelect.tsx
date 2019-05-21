b9dfee89303ab3925647be472a722be3cd7cc65e
Tejas Kumar <tejas@contiamo.com>
2019-05-21 13:28:47 +0200

    Adjust height of Select

--- src/DataTableSelect/DataTableSelect.tsx
+++ src/DataTableSelect/DataTableSelect.tsx
@@ -5,13 +5,13 @@ import styled from "../utils/styled"
 const StyledSelect = styled(Select)`
   margin: ${({ theme }) => -theme.space.medium}px ${({ theme }) => -theme.space.content}px;
   height: 100%;
-  min-height: fit-content;
+  min-height: 0;
   background: transparent;
   border: 0;
   font-weight: 400;
-  width: calc(100% + ${({ theme }) => theme.space.content * 2}px);
+  width: calc(100% + ${({ theme }) => theme.space.content * 2}px); /* 2 for left _and_ right. */
   max-width: none;
-` // * 2 for left _and_ right.
+`
 
 export const DataTableSelect: React.FC<SelectProps> = props => <StyledSelect {...props} />
 
