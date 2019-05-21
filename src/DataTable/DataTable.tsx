4d9a97ec1300fc78050a4a39fe3b50ea615e36b8
Tejas Kumar <tejas@contiamo.com>
2019-05-21 12:15:19 +0200

    Reimplement DataTable again

--- src/DataTable/DataTable.tsx
+++ src/DataTable/DataTable.tsx
@@ -3,71 +3,92 @@ import { FixedSizeList, ListChildComponentProps } from "react-window"
 
 import Message from "../Internals/Message/Message"
 import styled from "../utils/styled"
-import { truncate } from "../utils/truncate"
 
 export interface DataTableProps<T, P> {
+  /* The columns of our table. They are an array of header layers. */
   columns: T[][]
 
   /** A collection of rows for the table */
   rows: P[][]
 
-  /** Maximum character length in cells before truncation. Default `40`. */
-  maxCharactersInCell?: number
-
-  /** How high is each row? Default `30px` */
+  /** How high is each row (in pixels)? */
   rowHeight?: number
 
   /** Shall we include a footer? */
   footer?: React.ReactNode
 
   /** How much shall we restrict the height? */
-  height?: number
+  height?: string
+
+  /** How much shall we restrict the width? Default: unlimited */
+  width?: string
 
   /** How wide is each cell? Default `1fr` */
   cellWidth?: string
 }
 
-const Container = styled("div")`
-  width: 100%;
+const Container = styled("div", { shouldForwardProp: prop => prop !== "width" })<{ width: string }>`
+  position: relative;
+  width: ${({ width }) => width};
   border: 1px solid ${({ theme }) => theme.color.border.medium};
 `
 
-const HeadersContainer = styled("div")`
-  border-bottom: 1px solid ${({ theme }) => theme.color.border.gentle};
+const TableContainer = styled("div")<{ numColumns: number; numRows: number; columnWidth: string; rowHeight: number }>`
+  display: grid;
+  border-bottom: ${({ theme }) => `1px solid ${theme.color.border.gentle}`};
+  grid-template-columns: repeat(${({ numColumns, columnWidth }) => `${numColumns}, ${columnWidth}`});
+  grid-template-rows: repeat(${({ numRows, rowHeight }) => `${numRows}, ${rowHeight}px`});
 `
 
-const Row = styled("div")<{ numCells: number; cellWidth: string; isHeading?: boolean; isEven?: boolean }>`
+const Row = styled("div")<{ numCells: number; cellWidth: string; isEven?: boolean }>`
   display: grid;
   grid-template-columns: repeat(${({ numCells, cellWidth }) => `${numCells}, ${cellWidth}`});
-  background-color: ${({ theme, isHeading, isEven }) =>
-    isHeading ? theme.color.background.gentle : isEven ? theme.color.background.almostWhite : theme.color.white};
+  background-color: ${({ theme, isEven }) => (isEven ? theme.color.background.almostWhite : theme.color.white)};
 `
 
-const Cell = styled("div")<{ height: number; isHeading?: boolean }>`
+const Cell = styled("div", { shouldForwardProp: prop => prop !== "height" })<{ height: number }>`
   position: relative;
   display: flex;
   align-items: center;
-  box-shadow: 0 0 0 0.5px
-    ${({ theme, isHeading }) => (isHeading ? theme.color.border.gentle : theme.color.border.medium)};
+  box-shadow: 0 0 0 0.5px ${({ theme }) => theme.color.border.medium};
   height: ${({ height }) => height}px;
   font-family: ${({ theme }) => theme.font.family.code};
-  font-weight: ${({ theme, isHeading }) => (isHeading ? theme.font.weight.bold : theme.font.weight.regular)};
+  font-weight: ${({ theme }) => theme.font.weight.regular};
   padding: ${({ theme }) => theme.space.content}px;
-  color: ${({ theme, isHeading }) => (!isHeading ? theme.color.text.default : theme.color.text.dark)};
+  color: ${({ theme }) => theme.color.text.default};
 `
 
-/**
- * @todo
- * - [ ] handle keys
- * - [ ] add tests
- */
+const HeaderRow = styled("div")<{ rowHeight: number; numHeaders: number }>`
+  left: 0;
+  width: 100%;
+  height: ${({ rowHeight, numHeaders }) => rowHeight * numHeaders}px;
+  position: sticky;
+  grid-row: 1;
+  top: 0;
+  z-index: 100;
+`
+
+const HeaderCell = styled(Cell)`
+  background-color: ${({ theme }) => theme.color.background.gentle};
+  color: ${({ theme }) => theme.color.text.dark};
+  height: ${({ height }) => height}px;
+  font-weight: ${({ theme }) => theme.font.weight.bold};
+  box-shadow: 0 0 0 0.5px ${({ theme }) => theme.color.border.gentle};
+`
+
+const DataWrapper = styled("div")<{ numHeaders: number; rowHeight: number }>`
+  position: absolute;
+  width: 100%;
+  top: ${({ numHeaders, rowHeight }) => numHeaders * rowHeight + numHeaders * 2}px
+    /* ^ 1px border top and bottom per cell */;
+`
 
 const defaultProps = {
   rowHeight: 30,
   footer: null,
-  height: 500,
+  height: "500px",
   cellWidth: "1fr",
-  maxCharactersInCell: 40,
+  width: "100%",
 }
 
 export function DataTable<P, T>({
@@ -76,10 +97,10 @@ export function DataTable<P, T>({
   rowHeight = defaultProps.rowHeight,
   footer = defaultProps.footer,
   height = defaultProps.height,
+  width = defaultProps.width,
   cellWidth = defaultProps.cellWidth,
-  maxCharactersInCell = defaultProps.maxCharactersInCell,
 }: DataTableProps<T, P>) {
-  if (rows.length && rows[0].length !== columns[0].length) {
+  if (rows.length && rows[0].length !== columns.length) {
     return (
       <Message color="error">
         Invalid data: `rows` have different cardinality than `columns`. Please check both props and try again.
@@ -87,38 +108,66 @@ export function DataTable<P, T>({
     )
   }
 
-  const numCells = React.useMemo(() => columns[0].length, [columns])
+  const Table = React.useMemo(
+    () =>
+      React.memo(({ children, ...rest }) => (
+        <TableContainer
+          {...rest}
+          numColumns={columns.length}
+          numRows={rows.length}
+          columnWidth={cellWidth}
+          rowHeight={rowHeight}
+        >
+          {columns.map((headerRow, columnHeaderIndex) => (
+            <HeaderRow
+              key={`op-column-header-${columnHeaderIndex}-${performance.now()}`}
+              numHeaders={columns[0].length}
+              rowHeight={rowHeight}
+            >
+              {headerRow.map((cell, cellIndex) => (
+                <HeaderCell
+                  key={`op-column-header-cell-${columnHeaderIndex}-${cellIndex}-${performance.now()}`}
+                  height={rowHeight}
+                >
+                  {cell}
+                </HeaderCell>
+              ))}
+            </HeaderRow>
+          ))}
+
+          <DataWrapper numHeaders={columns[0].length} rowHeight={rowHeight}>
+            {children}
+          </DataWrapper>
+        </TableContainer>
+      )),
+    [columns, rows],
+  )
+
+  const numCells = React.useMemo(() => rows[0].length, [rows])
   const VirtualRow: React.FC<ListChildComponentProps> = React.useMemo(
     () =>
       React.memo(({ style, index }) => (
         <Row isEven={index % 2 === 0} style={{ ...style, height: rowHeight }} cellWidth={cellWidth} numCells={numCells}>
-          {rows[index].map(cell => (
-            <Cell height={rowHeight}>{truncate(maxCharactersInCell)(cell)}</Cell>
-          ))}
+          {rows[index] &&
+            rows[index].map(cell => (
+              <Cell key={`op-row-${index}-${performance.now()}`} height={rowHeight}>
+                {cell}
+              </Cell>
+            ))}
         </Row>
       )),
     [rows],
   )
 
   return (
-    <Container>
-      <HeadersContainer>
-        {columns.map(headerRow => (
-          <Row isHeading numCells={numCells} cellWidth={cellWidth}>
-            {headerRow.map(cell => (
-              <Cell height={rowHeight} isHeading>
-                {cell}
-              </Cell>
-            ))}
-          </Row>
-        ))}
-      </HeadersContainer>
+    <Container width={width}>
       <FixedSizeList
-        style={{ overflow: "overlay" }}
+        style={{ minWidth: "max-content" }}
         itemCount={rows.length}
         itemSize={rowHeight}
-        height={height || 500}
-        width="100%"
+        height={height}
+        width={width}
+        innerElementType={Table}
       >
         {VirtualRow}
       </FixedSizeList>
@@ -126,8 +175,5 @@ export function DataTable<P, T>({
     </Container>
   )
 }
-
-// @ts-ignore this only exists for styleguidist/ts-docgen
-DataTable.defaultProps = defaultProps
 
 export default React.memo(DataTable)
--- src/DataTable/README.md
+++ src/DataTable/README.md
@@ -2,18 +2,11 @@
 
 ```jsx
 import { Title, DataTable, DataTableSelect, DataTableFooter, Checkbox } from "@operational/components"
-const newRows = [
-  [Math.random(), Math.random(), Math.random()],
-  [Math.random(), Math.random(), Math.random()],
-  [Math.random(), Math.random(), Math.random()],
-  [Math.random(), Math.random(), Math.random()],
-  [Math.random(), Math.random(), Math.random()],
-  [Math.random(), Math.random(), Math.random()],
-  [Math.random(), Math.random(), Math.random()],
-]
+const getNewRows = (lastIndex = 0) =>
+  Array.from({ length: (lastIndex || 1) * 10 }, (_, i) => [i + lastIndex + 1, Math.random(), Math.random()])
 
 const MyComponent = () => {
-  const [rows, setRows] = React.useState(newRows)
+  const [rows, setRows] = React.useState(getNewRows())
 
   return (
     <>
@@ -21,42 +14,93 @@ const MyComponent = () => {
       <DataTable
         height={200}
         columns={[
-          ["Name", "Age", "Years of Employment"],
           [
+            "Name",
             <DataTableSelect options={[{ label: "Yes", value: "Yes" }]} value="Yes" />,
-            <DataTableSelect options={[{ label: "Yes", value: "Yes" }]} value="Yes" />,
-            <DataTableSelect options={[{ label: "Yes", value: "Yes" }]} value="Yes" />,
+            <Checkbox condensed label="is required" />,
           ],
           [
+            "Age",
+            <DataTableSelect options={[{ label: "Yes", value: "Yes" }]} value="Yes" />,
             <Checkbox condensed label="is required" />,
-            <Checkbox condensed label="is required" />,
+          ],
+          [
+            "Years of Employment",
+            <DataTableSelect options={[{ label: "Yes", value: "Yes" }]} value="Yes" />,
             <Checkbox condensed label="is required" />,
           ],
         ]}
         rows={rows}
         footer={
-          <DataTableFooter
-            onClick={() =>
-              setRows([
-                ...rows,
-                ...newRows,
-                ...rows,
-                ...newRows,
-                ...rows,
-                ...newRows,
-                ...rows,
-                ...newRows,
-                ...rows,
-                ...newRows,
-                ...rows,
-                ...newRows,
-              ])
-            }
-          >
+          <DataTableFooter onClick={() => setRows([...rows, ...getNewRows(rows[rows.length - 1][0])])}>
             Add more rows
           </DataTableFooter>
         }
       />
+    </>
+  )
+}
+
+;<MyComponent />
+```
+
+## Dynamic Columns
+
+```jsx
+import { DataTable, DataTableSelect, Checkbox } from "@operational/components"
+
+const columnsReducer = (columns, action) => {
+  switch (action.type) {
+    case "[add column]":
+      return [...columns, action.column]
+    case "[remove column]":
+      return columns.filter(column => column[0] !== action.name)
+    default:
+      return columns
+  }
+}
+
+const rowsReducer = (rows, action) => {
+  switch (action.type) {
+    case "[add column]":
+      return rows.map(row => [...row, Math.random()])
+    case "[remove column]":
+      return rows.map(row => [...row.slice(0, row.length - 1)])
+    default:
+      return rows
+  }
+}
+
+const MyComponent = () => {
+  const [rows, rowsDispatch] = React.useReducer(rowsReducer, [["hello"], ["hello"], ["hello"], ["hello"], ["hello"]])
+  const [columns, columnsDispatch] = React.useReducer(columnsReducer, [["Tejas", "nothing"]])
+
+  const makeColumn = React.useCallback(
+    (name = Math.random()) => [
+      name,
+      <button
+        onClick={() => {
+          columnsDispatch({ type: "[remove column]", name })
+          rowsDispatch({ type: "[remove column]" })
+        }}
+      >
+        x
+      </button>,
+    ],
+    [columns],
+  )
+
+  return (
+    <>
+      <DataTable height={200} columns={columns} rows={rows} />
+      <button
+        onClick={() => {
+          columnsDispatch({ type: "[add column]", column: makeColumn() })
+          rowsDispatch({ type: "[add column]" })
+        }}
+      >
+        Add Column
+      </button>
     </>
   )
 }
