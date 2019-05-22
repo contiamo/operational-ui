2628a34861c2f9777483039c6ffae81abe96a779
Tejas Kumar <tejas@contiamo.com>
2019-05-22 11:39:21 +0200

    Warn only once when trying to truncate non-primitive

--- src/utils/truncate.ts
+++ src/utils/truncate.ts
@@ -1,3 +1,5 @@
+let didWarn = false
+
 export const truncate = (length: number) => (thingToTruncate: any) => {
   if (["string", "number"].includes(typeof thingToTruncate)) {
     const stringifiedValue = String(thingToTruncate)
@@ -7,6 +9,14 @@ export const truncate = (length: number) => (thingToTruncate: any) => {
     } else {
       return truncatedValue + "…"
     }
+  }
+
+  if (!didWarn && process.env.NODE_ENV !== "development") {
+    console.warn(
+      "Attempting to truncate something that is noxt a JS primitive: this will fail for functions, objects and arrays. Please reconsider the item you are truncating:",
+      thingToTruncate,
+    )
+    didWarn = true
   }
 
   return thingToTruncate
