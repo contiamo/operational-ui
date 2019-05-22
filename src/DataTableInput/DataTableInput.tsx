a986bc685e64712d9b656e2d8a701cdaa83afe7e
Tejas Kumar <tejas@contiamo.com>
2019-05-22 17:44:34 +0200

    Update DataTableInput style

--- src/DataTableInput/DataTableInput.tsx
+++ src/DataTableInput/DataTableInput.tsx
@@ -3,16 +3,12 @@ import Input, { InputProps } from "../Input/Input"
 import styled from "../utils/styled"
 
 const StyledInput = styled(Input)`
+  border: 0;
   margin: ${({ theme }) => -theme.space.medium}px ${({ theme }) => -theme.space.content}px;
   height: 100%;
   min-height: 0;
-  background: transparent;
-  border: 0;
-  font-weight: 400;
-  width: calc(100% + ${({ theme }) => theme.space.content * 2}px); /* 2 for left _and_ right. */
-  max-width: none;
 ` as typeof Input
 
-export const DataTableInput: React.FC<InputProps> = props => <StyledInput {...props} />
+export const DataTableInput: React.FC<InputProps> = props => <StyledInput fullWidth {...props} />
 
 export default DataTableInput
