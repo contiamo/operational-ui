4d801626f32626603d0fd04bcd700d7d48e2d0a9
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-05-24 13:07:53 -0400

    Update itShouldOnlyMountOnce.tsx

--- src/__testUtils__/itShouldOnlyMountOnce.tsx
+++ src/__testUtils__/itShouldOnlyMountOnce.tsx
@@ -1,33 +1,43 @@
-import { mount } from "enzyme"
-import * as React from "react"
+import React from "react"
+import { cleanup, fireEvent, render } from "react-testing-library"
+
+interface ChildProps {
+  cb: () => void
+}
 
 /**
- * This calls the passed function on every `componentDidMount`
- * This is how we track is the parent un-mounts.
- * We can use hooks here because were cool.
+ * This calls the passed callback on every `componentDidMount`
+ * This is how we track if the parent un-mounts.
  *
  */
-
-function Child({ mockFN }: { mockFN: jest.Mock }) {
+function Child({ cb }: ChildProps) {
   React.useEffect(() => {
-    mockFN()
+    cb()
   }, [])
 
   return <div />
 }
 
+type WrapperProps = ChildProps & { Comp: React.FunctionComponent<any> }
+
 /**
- * Must be a class in order to update state with enzyme.
  * We use this to re-render its child on every state change
  */
-class TestWrapper extends React.Component<{ comp: React.FunctionComponent<{ count: number }> }, { count: number }> {
-  public state = {
-    count: 1,
-  }
+const Wrapper = ({ cb, Comp }: WrapperProps) => {
+  const [count, setCount] = React.useState(1)
+  const handleClick = () => setCount(count + 1)
 
-  public render() {
-    return this.props.comp({ count: this.state.count })
-  }
+  return (
+    <div>
+      <button data-testid="triggerStateChange" onClick={handleClick}>
+        Increase
+      </button>
+      <input data-testid="verifyStateChange" value={count} readOnly />
+      <Comp>
+        <Child cb={cb} />
+      </Comp>
+    </div>
+  )
 }
 
 /**
@@ -39,29 +49,18 @@ class TestWrapper extends React.Component<{ comp: React.FunctionComponent<{ coun
  *
  *  ![TestHack](https://media1.tenor.com/images/7da78fe87cf5a457fbec3406e82d6cb3/tenor.gif)
  */
+
 export const itShouldOnlyMountOnce = (componentName: string, Component: React.FunctionComponent<any>) => {
   const mockFn = jest.fn()
-
-  const Inner = ({ count }: { count: number }) => {
-    return (
-      <React.Fragment>
-        <button>Count:{count}</button>
-        <Component>
-          <Child mockFN={mockFn} />
-        </Component>
-      </React.Fragment>
-    )
-  }
-
+  afterEach(cleanup)
   it(`[${componentName}] should only mount once`, () => {
-    const m = mount(<TestWrapper comp={Inner} />)
-    /// Lets Update the state 4 Times
-    m.setState({ count: 2 })
-    m.setState({ count: 3 })
-    m.setState({ count: 4 })
-    m.setState({ count: 5 })
-
-    /// Component should have only mounted once
-    expect(mockFn).toBeCalledTimes(1)
+    const w = render(<Wrapper cb={mockFn} Comp={Component} />)
+    // First Verify State Does Change
+    expect((w.getByTestId("verifyStateChange") as HTMLInputElement).value).toEqual("1")
+    fireEvent.click(w.getByTestId("triggerStateChange"))
+    // Input should now be 2
+    expect((w.getByTestId("verifyStateChange") as HTMLInputElement).value).toEqual("2")
+    // Now check that mockFN was only called once
+    expect(mockFn).toHaveBeenCalledTimes(1)
   })
 }
