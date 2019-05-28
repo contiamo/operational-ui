14c4a398c01c45e6373e8357ff7befc40ef0875b
stereobooster <stereobooster@gmail.com>
2019-05-28 12:20:51 +0200

    update snapshots

--- src/AccordionSection/AccordionSection.tsx
+++ src/AccordionSection/AccordionSection.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-// import { heightOfAccordionHeader } from "../Accordion/Accordion"
 import Icon from "../Icon/Icon"
 import { DefaultProps } from "../types"
 import { useUniqueId } from "../useUniqueId"
