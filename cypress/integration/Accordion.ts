87fafb0f21f3cbf42e8f7131f223a4daac1ec5a2
stereobooster <stereobooster@gmail.com>
2019-05-29 11:43:49 +0200

    Use tabs in cypress test

--- cypress/integration/Accordion.ts
+++ cypress/integration/Accordion.ts
@@ -2,6 +2,7 @@ describe("Accordion", () => {
   before(() => {
     cy.visit("/#!/Accordion")
   })
+
   it("should exist", () => {
     cy.get("[data-cy=operational-ui__Accordion]")
   })
@@ -40,37 +41,41 @@ describe("Accordion", () => {
       .should("have.attr", "hidden")
   })
 
-  it("opens section if you click corresponding header", () => {
+  it("closes section if you focus and press spacebar at corresponding header", () => {
     cy.get("[data-cy=operational-ui__Accordion]")
       .find("[role=region]")
-      .last()
-      .should("have.attr", "hidden")
+      .first()
+      .should("not.have.attr", "hidden")
 
-    cy.get("[data-cy=operational-ui__Accordion]")
-      .find("[role=button]")
-      .last()
+    cy.get("body")
+      .tab() // Show all components
+      .tab() // Props & methods
+      .tab() // link
+      .tab() // First header in Accordion
       .type(" ")
 
     cy.get("[data-cy=operational-ui__Accordion]")
       .find("[role=region]")
-      .last()
-      .should("not.have.attr", "hidden")
+      .first()
+      .should("have.attr", "hidden")
   })
 
-  it("closes section if you click corresponding header", () => {
+  it("opens section if you focus and press spacebar at corresponding header", () => {
     cy.get("[data-cy=operational-ui__Accordion]")
       .find("[role=region]")
-      .last()
-      .should("not.have.attr", "hidden")
+      .first()
+      .should("have.attr", "hidden")
 
-    cy.get("[data-cy=operational-ui__Accordion]")
-      .find("[role=button]")
-      .last()
+    cy.get("body")
+      .tab()
+      .tab()
+      .tab()
+      .tab()
       .type(" ")
 
     cy.get("[data-cy=operational-ui__Accordion]")
       .find("[role=region]")
-      .last()
-      .should("have.attr", "hidden")
+      .first()
+      .should("not.have.attr", "hidden")
   })
 })
