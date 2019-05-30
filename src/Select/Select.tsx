96e540239189cb8e37bc661d6744c235bcf9b168
Tejas Kumar <tejas@contiamo.com>
2019-05-30 16:03:24 +0200

    Initial Select behavior with Input and ContextMenu

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -1,5 +1,9 @@
 import * as React from "react"
 import { DefaultProps } from "../types"
+import { IContextMenuItem } from "../ContextMenu/ContextMenu.Item"
+import ContextMenu from "../ContextMenu/ContextMenu"
+import styled from "../utils/styled"
+import Input from "../Input/Input"
 
 export type Value = number | string
 
@@ -33,6 +37,79 @@ export interface SelectProps extends DefaultProps {
   naked?: boolean
 }
 
-export const Select: React.FC<SelectProps> = () => <></>
+const borderRadius = 2
+
+const Container = styled("div")`
+  display: flex;
+  flex-direction: column;
+`
+
+const Combobox = styled("div")`
+  display: grid;
+  grid-template-columns: auto 40px;
+  grid-gap: 1px;
+  align-items: stretch;
+  border: 1px solid ${({ theme }) => theme.color.border.default};
+  border-radius: ${borderRadius}px;
+`
+
+const SelectInput = styled(Input)`
+  width: fit-content;
+  border: 0;
+  background: transparent;
+  color: currentColor;
+  cursor: inherit;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+`
+
+const DropdownButton = styled("div")<{ isOpen: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 0 0 1px ${({ theme, isOpen }) => (isOpen ? theme.color.primary : "transparent")};
+  border-top-right-radius: ${borderRadius}px;
+  border-bottom-right-radius: ${borderRadius}px;
+
+  ::after {
+    content: "";
+    width: 0;
+    height: 0;
+    border: 4px solid transparent;
+    border-top-color: ${({ theme, isOpen }) => (isOpen ? theme.color.primary : theme.color.border.default)};
+    transform: ${({ isOpen }) => (isOpen ? `translateY(-2px) rotate(180deg)` : `translateY(2px)`)};
+  }
+`
+
+export const Select: React.FC<SelectProps> = ({ options, value, onChange }) => {
+  const items = React.useMemo(
+    () =>
+      options.map(
+        (option): IContextMenuItem => ({
+          ...option,
+          onClick: () => {
+            if (onChange) {
+              onChange(option.value, option.value)
+            }
+          },
+          label: option.label ? option.label : "",
+        }),
+      ),
+    [options],
+  )
+
+  return (
+    <ContextMenu items={items}>
+      {isOpen => (
+        <Container>
+          <Combobox>
+            <SelectInput value={String(value)} />
+            <DropdownButton isOpen={isOpen} />
+          </Combobox>
+        </Container>
+      )}
+    </ContextMenu>
+  )
+}
 
 export default Select
