3c365fa41bba8fa1838c14490cd54ac93e78ae24
Fabien BERNARD <fabien0102@hotmail.com>
2019-06-05 10:35:44 +0200

    Add new icons

---
+++ build-icons.ts
@@ -0,0 +1,130 @@
+import svgr from "@svgr/core"
+import { readdirSync, readFileSync, mkdirSync, writeFileSync, existsSync } from "fs"
+import * as rimraf from "rimraf"
+import { join, parse } from "path"
+import * as ProgressBar from "progress"
+import * as program from "commander"
+import * as chokidar from "chokidar"
+
+program
+  .version(require("./package.json").version)
+  .option("-o, --output [relative path]", "Output folder")
+  .option("-i, --input [relative path]", "Input folder")
+  .option("-c, --clean", "Clean output folder before processing")
+  .option("-w, --watch", "Watch filesystem for rebuild")
+  .parse(process.argv)
+
+const inputFolder = program.input || "./icons"
+const outputFolder = program.output || "./src/Icon"
+
+// Make sure the output folder is clean
+if (program.clean) {
+  rimraf.sync(outputFolder)
+}
+
+// Ensure the output folder exists
+if (!existsSync(outputFolder)) {
+  mkdirSync(outputFolder)
+}
+
+const run = () => {
+  const files = readdirSync(inputFolder).filter(i => /\.svg$/.exec(i))
+
+  // Create a progress bar for a pretty output
+  const progressBar = new ProgressBar("  creating react components [:bar] :percent", files.length)
+
+  /**
+   * Component template.
+   *
+   * ref: https://www.smooth-code.com/open-source/svgr/docs/typescript/
+   */
+  function template({ template }, opts, { componentName, jsx }) {
+    const typeScriptTpl = template.smart({ plugins: ["typescript"] })
+    return typeScriptTpl.ast`
+    import * as React from "react";
+    import constants, { expandColor } from "../utils/constants";
+    import { IconProps } from "./";
+    export const ${componentName} = ({size = 18, color, left, right, ...props}: React.SVGProps<SVGSVGElement> & IconProps) => {
+      const iconColor: string = expandColor(constants, color) || "currentColor";
+      const style = {
+        marginLeft: right ? 8 : 0, // theme.space.small
+        marginRight: left ? 8 : 0, // theme.space.small
+        cursor: Boolean(props.onClick) ? "pointer" : "default",
+        transition: "fill .075s ease"
+      };
+  
+      return ${jsx}
+    };
+  `
+  }
+
+  // Copy everything into this repository
+  files.forEach(fileName => {
+    const { name } = parse(fileName)
+    const svgCode = readFileSync(join(inputFolder, fileName), "utf-8")
+    const output = svgr.sync(
+      svgCode,
+      {
+        prettier: true,
+        svgo: true,
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx", "@svgr/plugin-prettier"],
+        svgProps: {
+          fill: "{iconColor}",
+          width: "{size}",
+          height: "{size}",
+          style: "{style}",
+        },
+        template,
+      },
+      { componentName: `Icon${name}` },
+    )
+    writeFileSync(join(outputFolder, `Icon${name}.tsx`), output)
+    progressBar.tick()
+  })
+
+  // Create index.ts
+  const index = `export interface IconPropsBase {
+    /**
+     * Size
+     *
+     * @default 18
+     */
+    size?: number;
+    /** Icon color, specified as a hex, or a color name (info, success, warning, error) */
+    color?: string;
+  }
+  
+export type IconProps =
+  | (IconPropsBase & {
+      left?: never;
+      /**
+       * Indicates that this component is right of other content, and adds an appropriate left margin.
+       */
+      right?: boolean;
+    })
+  | (IconPropsBase & {
+      /**
+       * Indicates that this component is left of other content, and adds an appropriate right margin.
+       */
+      left?: boolean;
+      right?: never;
+    });
+  
+${files
+  .map(fileName => {
+    const { name } = parse(fileName)
+
+    return `export * from "./Icon${name}";`
+  })
+  .join("\n")}
+  `
+
+  writeFileSync(join(outputFolder, "index.ts"), index)
+}
+
+run()
+
+// Watch mode (svg folder only)
+if (program.watch) {
+  chokidar.watch(["svg/**.svg"]).on("change", run)
+}
