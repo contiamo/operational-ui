74544920536e5ac17a1092400249c961d5d71f56
Mikhail Potomin <mpotomin@gmail.com>
2019-06-06 18:20:41 +0200

    Testing header links

--- cypress/integration/Stepper.ts
+++ cypress/integration/Stepper.ts
@@ -1,8 +1,18 @@
 import { tabPastStyleguidistLinks } from "../support/helpers"
 
 describe("Steeper", () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit("/#!/Stepper")
+  })
+
+  it("allows jumping to the next step by clicking the inactive header", () => {
+    cy.get("[data-cy=operational-ui__Stepper]")
+      .find("[role=tab]")
+      .last()
+      .click()
+
+    tabPastStyleguidistLinks()
+    cy.focused().contains("Import")
   })
 
   it("allows cycling through provided steps", () => {
