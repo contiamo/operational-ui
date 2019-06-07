aca513f8a2c97c5dd532d5723ddbbbe5e5a953e3
Mikhail Potomin <mpotomin@gmail.com>
2019-06-07 14:45:23 +0200

    Iterating on Stepper layout

--- src/Stepper/Stepper.tsx
+++ src/Stepper/Stepper.tsx
@@ -97,6 +97,33 @@ const StepLabel = styled("div")`
 `
 StepLabel.displayName = "StepLabel"
 
+const NewSteps = styled("ul")<{ steps: number }>`
+  label: Steps;
+  display: flex;
+  justify-items: center;
+  margin: 0;
+`
+NewSteps.displayName = "Steps"
+
+const NewStep = styled("li")<{ stepState: StepState }>`
+  label: Step;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-weight: ${({ theme, stepState }) => (stepState === "active" ? "bold" : theme.font.weight.regular)};
+  font-size: ${({ theme }) => theme.font.size.body}px;
+  color: ${({ theme, stepState }) => {
+    if (stepState === "active") return theme.color.text.dark
+    if (stepState === "completed") return theme.color.text.lighter
+    return theme.color.text.lightest
+  }};
+  cursor: pointer;
+`
+NewStep.defaultProps = { role: "tab" }
+NewStep.displayName = "Step"
+
 function getStepState(index: number, activeIndex?: number): StepState {
   if (activeIndex === undefined) return "inactive"
   if (index < activeIndex) return "completed"
@@ -104,13 +131,69 @@ function getStepState(index: number, activeIndex?: number): StepState {
   return "inactive"
 }
 
+const ballSize = 24
+const translateY = ballSize / 2
+
+const CheeseMeister = styled("div")<{ stepState: StepState; number: number; color: StepperProps["stepColor"] }>`
+  position: relative;
+  width: 100%;
+  font-weight: ${({ theme }) => theme.font.weight.medium};
+  text-align: center;
+  overflow: hidden;
+  
+  /** Ball with number in it */
+  ::before {
+    content: '${({ number }) => number}';
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto ${({ theme }) => theme.space.small}px;
+    width: ${ballSize}px;
+    height: ${ballSize}px;
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.color.primary};
+    color: ${({ theme }) => theme.color.white};
+  z-index: 1;
+  }
+
+  /** Separator line */
+  ::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    top: 0;
+    transform: translateY(${translateY}px);
+    display: block;
+    height: 1px;
+    width: 100%;
+    background-color: ${({ theme }) => theme.color.border.default};
+  }
+
+  :first-of-type::after,
+  :last-of-type::after {
+    width: 50%;
+  }
+
+:first-of-type::after {
+  transform: translate(50%, ${translateY}px)
+}
+
+:last-of-type::after {
+  transform: translate(-50%, ${translateY}px)
+}
+
+`
+
 const Stepper: React.FC<StepperProps> = props => {
   const { steps, stepColor, onStepChange, activeSlideIndex, ...rest } = props
   return (
     <div data-cy="operational-ui__Stepper" {...rest}>
-      <Steps data-cy="operational-ui__Stepper-steps">
+      <NewSteps steps={steps.length} data-cy="operational-ui__Stepper-steps">
         {steps.map(({ title }, index) => (
-          <Step
+          <CheeseMeister
             data-cy={`operational-ui__Stepper__step-${index}`}
             tabIndex={index === activeSlideIndex ? 0 : -1}
             aria-selected={index === activeSlideIndex}
@@ -124,10 +207,10 @@ const Stepper: React.FC<StepperProps> = props => {
               }
             }}
           >
-            <StepLabel>{title}</StepLabel>
-          </Step>
+            {title}
+          </CheeseMeister>
         ))}
-      </Steps>
+      </NewSteps>
       <StepContent data-cy="operational-ui__Stepper-content">{steps[activeSlideIndex!].content}</StepContent>
     </div>
   )
