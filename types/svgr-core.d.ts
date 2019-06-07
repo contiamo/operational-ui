fd7addfcc2e84109732a60f199d744ede2ec1e2d
Fabien BERNARD <fabien0102@hotmail.com>
2019-06-07 11:31:40 +0200

    Move svg.core.d.ts

--- types/svgr-core.d.ts
+++ types/svgr-core.d.ts
@@ -3,77 +3,69 @@ declare module "@svgr/core" {
     /**
      * Replace SVG "width" and "height" value by "1em" in order to make SVG size inherits from text size.
      */
-    icon: boolean;
+    icon: boolean
     /**
      * Specify a custom extension for generated files.
      */
-    ext: string;
+    ext: string
     /**
      * Remove width and height from root SVG tag.
      */
-    dimensions: boolean;
+    dimensions: boolean
     /**
      * Use Prettier to format JavaScript code output.
      */
-    prettier: boolean;
+    prettier: boolean
     /**
      * Specify Prettier config. See Prettier options.
      */
-    prettierConfig: any;
+    prettierConfig: any
     /**
      * Use SVGO to optimize SVG code before transforming it into a component.
      */
-    svgo: boolean;
+    svgo: boolean
     /**
      * Specify SVGO config. See SVGO options.
      */
-    svgoConfig: any;
+    svgoConfig: any
     /**
      * Setting this to true will forward ref to the root SVG tag.
      */
-    ref: boolean;
+    ref: boolean
     /**
      * Replace an attribute value by an other. The main usage of this option is to change an icon color to "currentColor" in order to inherit from text color.
      */
-    replaceAttrValues: Record<string, string>;
+    replaceAttrValues: Record<string, string>
     /**
      * Add props to the root SVG tag.
      */
-    svgProps: Record<string, string>;
+    svgProps: Record<string, string>
     /**
      * Add title tag via title property.
      */
-    titleProp: boolean;
+    titleProp: boolean
     /**
      * Specify a template file (CLI) or a template function (API) to use. For an example of template, see the default one.
      */
-    template: Function;
+    template: Function
     /**
      * List of plugins.
      */
-    plugins: string[];
+    plugins: string[]
   }
 
   interface State {
     /**
      * Specify a react component name.
      */
-    componentName: string;
+    componentName: string
   }
 
-  function svgr(
-    svgCode: string,
-    config?: Partial<Config>,
-    state?: Partial<State>
-  ): Promise<string>;
+  function svgr(svgCode: string, config?: Partial<Config>, state?: Partial<State>): Promise<string>
 
   namespace svgr {
-    function sync(
-      svgCode: string,
-      config?: Partial<Config>,
-      state?: Partial<State>
-    ): string;
+    function sync(svgCode: string, config?: Partial<Config>, state?: Partial<State>): string
   }
 
-  export default svgr;
+  export default svgr
 }
