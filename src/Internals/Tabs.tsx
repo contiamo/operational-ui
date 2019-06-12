673d102e0389441a0abe4f83ea2770b49b085e96
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2019-06-12 15:45:30 +0200

    Add a11y functionality to page tabs.

--- src/Internals/Tabs.tsx
+++ src/Internals/Tabs.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react"
 import Spinner from "../Spinner/Spinner"
 import styled from "../utils/styled"
 import { IconComponentType } from "../Icon/Icon"
+import { inputFocus } from "../utils"
 
 export interface Tab {
   name: string
@@ -43,26 +44,40 @@ const TabsBar = styled("div")(({ theme }) => ({
   },
 }))
 
-const Tab = styled("div")<{ active?: boolean }>(({ theme, active }) => ({
+const SingleTab = styled("div")<{ active?: boolean }>(({ theme, active }) => ({
   width: 150,
   height: "100%",
   display: "flex",
   alignItems: "center",
-  backgroundColor: active ? theme.color.white : theme.color.background.lighter,
-  color: active ? theme.color.text.action : theme.color.text.dark,
   fontFamily: theme.font.family.main,
   fontSize: theme.font.size.small,
-  fontWeight: active ? theme.font.weight.bold : theme.font.weight.regular,
   padding: `0px ${theme.space.element}px`,
   border: `1px solid ${theme.color.border.select}`,
   zIndex: 2,
-  borderBottomColor: active ? theme.color.white : theme.color.border.select,
+  ...(active
+    ? {
+        backgroundColor: theme.color.white,
+        color: theme.color.text.action,
+        fontWeight: theme.font.weight.bold,
+        borderBottomColor: theme.color.white,
+      }
+    : {
+        backgroundColor: theme.color.background.lighter,
+        color: theme.color.text.dark,
+        fontWeight: theme.font.weight.regular,
+        borderBottomColor: theme.color.border.select,
+      }),
   borderRightWidth: 0,
   ":last-child": {
     borderRightWidth: 1,
   },
   ":hover": {
     cursor: "pointer",
+  },
+  ":focus": {
+    ...inputFocus({ theme, isError: false }),
+    borderRightWidth: 1,
+    zIndex: 3,
   },
 }))
 
@@ -85,6 +100,8 @@ const getTabIndexByName = (tabs: Tab[], tabName?: string): number => {
 const Tabs = ({ onTabChange, tabs, activeTabName, children }: Props) => {
   const activeTabIndex = getTabIndexByName(tabs, activeTabName)
   const [activeTab, setActiveTab] = useState(activeTabIndex)
+  const [isMouseDown, setMouseDown] = useState(false)
+
   useEffect(() => {
     setActiveTab(activeTabIndex)
   }, [activeTabIndex])
@@ -99,25 +116,79 @@ const Tabs = ({ onTabChange, tabs, activeTabName, children }: Props) => {
     [onTabChange, tabs, setActiveTab],
   )
 
+  const onKeyDown = useCallback(
+    (event: React.KeyboardEvent) => {
+      const tabElements: NodeListOf<HTMLElement> = document.querySelectorAll('[role="tab"]')
+      let newIndex: number
+      switch (event.key) {
+        case "ArrowRight":
+          event.preventDefault()
+          newIndex = activeTab + 1 >= tabs.length ? 0 : activeTab + 1
+          tabElements[newIndex].focus()
+          onTabClick(newIndex)
+          break
+        case "ArrowLeft":
+          event.preventDefault()
+          newIndex = activeTab - 1 < 0 ? tabs.length - 1 : activeTab - 1
+          tabElements[newIndex].focus()
+          onTabClick(newIndex)
+          break
+        case "Home":
+          event.preventDefault()
+          newIndex = tabs.length - 1
+          tabElements[newIndex].focus()
+          // Activate last tab
+          onTabClick(newIndex)
+          break
+        case "End":
+          event.preventDefault()
+          newIndex = 0
+          tabElements[newIndex].focus()
+          // Activate first tab
+          onTabClick(0)
+          break
+      }
+    },
+    [activeTab, onTabClick],
+  )
+
   // Work around: wrap return in fragment- to prevent type error and not having to change childrens return type
   // https://github.com/Microsoft/TypeScript/issues/21699
   return (
     <>
       {children({
         tabsBar: (
-          <TabsBar>
+          <TabsBar role={"tablist"} onKeyDown={onKeyDown}>
             {tabs
               .filter(({ hidden }) => !hidden)
-              .map((tab, index: number) => (
-                <Tab key={index} active={activeTab === index} onClick={() => onTabClick(index)}>
-                  {tab.loading ? (
-                    <Spinner left size={14} />
-                  ) : (
-                    tab.icon && React.createElement(tab.icon, { size: 14, color: tab.iconColor, left: true })
-                  )}
-                  <TabName>{tab.name}</TabName>
-                </Tab>
-              ))}
+              .map((tab, index: number) => {
+                const isActive: boolean = activeTab === index
+                return (
+                  <SingleTab
+                    key={index}
+                    role={"tab"}
+                    active={isActive}
+                    aria-selected={isActive}
+                    tabIndex={isActive ? 0 : -1}
+                    onClick={() => onTabClick(index)}
+                    onKeyDown={onKeyDown}
+                    onFocus={e => {
+                      if (isMouseDown) {
+                        e.target.blur()
+                      }
+                    }}
+                    onMouseDown={() => setMouseDown(true)}
+                    onMouseUp={() => setMouseDown(false)}
+                  >
+                    {tab.loading ? (
+                      <Spinner left size={14} />
+                    ) : (
+                      tab.icon && React.createElement(tab.icon, { size: 14, color: tab.iconColor, left: true })
+                    )}
+                    <TabName>{tab.name}</TabName>
+                  </SingleTab>
+                )
+              })}
           </TabsBar>
         ),
         activeChildren: tabs[activeTab].children,
