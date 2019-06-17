4ec3f8e96cf01ae4ed95c01ff01dbffa187feca0
Tejas Kumar <tejas@tejas.qa>
2019-06-17 07:41:09 +0200

    Add comment about apparently redudant tabIndex

--- src/Breadcrumb/Breadcrumb.tsx
+++ src/Breadcrumb/Breadcrumb.tsx
@@ -28,7 +28,7 @@ const Breadcrumb: React.SFC<BreadcrumbProps> = ({ to, icon: Icon, onClick, ...pr
     {ctx => (
       <Container
         {...props}
-        tabIndex={0}
+        tabIndex={0} // for cypress
         href={to}
         onClick={(ev: React.SyntheticEvent<Node>) => {
           if (onClick) {
