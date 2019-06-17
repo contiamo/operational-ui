3a761e3119dfd4780a8064057e01494a9f5cf982
Imogen Mason <imogen.n.fletcher@gmail.com>
2019-06-17 10:41:04 +0200

    **Breaking:** Breadcrumbs (#1020)
    * Implement new breadcrumb design.
    
    * Make caret clickable.
    
    * Tweak breadcrumbs for accessibility.
    
    * Add e2e breadcrumb tests.
    
    * Add comment.
    
    * Fix breadcrumb cypress tests.
    
    * Add comment about apparently redudant tabIndex
    
    * Update src/Breadcrumbs/Breadcrumbs.tsx

--- src/Breadcrumb/Breadcrumb.tsx
+++ src/Breadcrumb/Breadcrumb.tsx
@@ -3,62 +3,49 @@ import { IconComponentType } from "../Icon/Icon"
 import OperationalContext from "../OperationalContext/OperationalContext"
 import { DefaultProps } from "../types"
 import { darken, isModifiedEvent, isOutsideLink } from "../utils"
-import { OperationalStyleConstants } from "../utils/constants"
 import styled from "../utils/styled"
 
 export interface BreadcrumbProps extends DefaultProps {
   /** Navigation property à la react-router <Link/> */
-  to?: string
+  to: string
   onClick?: (ev?: React.SyntheticEvent<React.ReactNode>) => void
   children?: React.ReactNode
   icon?: IconComponentType
+  /** Optionally moves icon from default position on right, to before the text, including appropriate padding */
+  iconLeft?: boolean
 }
 
-const containerStyles = {
+const Container = styled("a")(({ theme }) => ({
+  color: theme.color.primary,
   label: "breadcrumb",
-  "& svg": {
-    marginLeft: 4,
-  },
-}
-
-const Container = styled("span")(containerStyles)
-
-const ContainerLink = styled("a")(containerStyles, ({ theme }: { theme: OperationalStyleConstants }) => ({
-  "&:link, &:visited": {
-    color: theme.color.primary,
-    borderBottom: "1px solid currentColor",
-    "&:hover": {
-      color: darken(theme.color.primary, 5),
-    },
+  "&:hover": {
+    color: darken(theme.color.primary, 5),
   },
 }))
 
-const Content = styled("span")()
-
-const Breadcrumb: React.SFC<BreadcrumbProps> = ({ to, icon: Icon, onClick, ...props }) => {
-  const ContainerComponent: any = to ? ContainerLink : Container
-  return (
-    <OperationalContext>
-      {ctx => (
-        <ContainerComponent
-          {...props}
-          href={to}
-          onClick={(ev: React.SyntheticEvent<Node>) => {
-            if (onClick) {
-              onClick(ev)
-            }
-            if (!isModifiedEvent(ev) && ctx.pushState && to && !isOutsideLink(to)) {
-              ev.preventDefault()
-              ctx.pushState(to)
-            }
-          }}
-        >
-          <Content>{props.children}</Content>
-          {Icon && <Icon size={12} />}
-        </ContainerComponent>
-      )}
-    </OperationalContext>
-  )
-}
+const Breadcrumb: React.SFC<BreadcrumbProps> = ({ to, icon: Icon, onClick, ...props }) => (
+  <OperationalContext>
+    {ctx => (
+      <Container
+        {...props}
+        tabIndex={0} // for cypress
+        href={to}
+        onClick={(ev: React.SyntheticEvent<Node>) => {
+          if (onClick) {
+            onClick(ev)
+          }
+          if (!isModifiedEvent(ev) && ctx.pushState && to && !isOutsideLink(to)) {
+            ev.preventDefault()
+            ctx.pushState(to)
+          }
+        }}
+      >
+        {Icon && props.iconLeft && <Icon size={12} left />}
+        <span>{props.children}</span>
+        {Icon && !props.iconLeft && <Icon size={12} right />}
+      </Container>
+    )}
+  </OperationalContext>
+)
 
 export default Breadcrumb
--- src/Breadcrumbs/Breadcrumbs.tsx
+++ src/Breadcrumbs/Breadcrumbs.tsx
@@ -1,43 +1,45 @@
 import * as React from "react"
 import { DefaultProps } from "../types"
 import styled from "../utils/styled"
+import { CaretLeftIcon } from "../Icon/Icon"
 
 export interface BreadcrumbsProps extends DefaultProps {
   /** Children as `Breadcrumb` elements */
   children?: React.ReactNode
 }
 
-const Container = styled("div")({
+const Container = styled("nav")(({ theme }) => ({
   label: "breadcrumbs",
+  color: theme.color.text.action,
   "& > *": {
-    display: "inline-block",
-  },
-  "& a": {
-    textDecoration: "none",
-    color: "inherit",
-  },
-})
-
-const Slash = styled("span")(({ theme }) => ({
-  display: "inline-block",
-  margin: `0 ${theme.space.medium}px`,
-  color: theme.color.text.lightest,
-  ":first-child": {
-    marginLeft: 0,
+    verticalAlign: "middle",
   },
 }))
+
+const Slash = styled("span")(({ theme }) => ({
+  margin: `0 ${theme.space.content}px`,
+  color: theme.color.text.lightest,
+}))
+
+const addCaret = (item: React.ReactElement) => React.cloneElement(item, { icon: CaretLeftIcon, iconLeft: true })
 
 /**
  * Intersperse slashes between the children (`<Breadcrumb />` elements)
  * Curried first argument is necessary to give unique auto-incrementing
  *  keys to the slash elements.
  */
-
 const intersperseSlashes = (index: number) => ([head, ...tail]: React.ReactNode[]): React.ReactNode[] =>
-  head ? [<Slash key={`divider-${index}`}>/</Slash>, head, ...intersperseSlashes(index + 1)(tail)] : []
+  head && tail.length
+    ? [head, <Slash key={`divider-${index}`}>/</Slash>, ...intersperseSlashes(index + 1)(tail)]
+    : [head]
 
-const Breadcrumbs: React.SFC<BreadcrumbsProps> = props => (
-  <Container {...props}>{intersperseSlashes(0)(React.Children.toArray(props.children))}</Container>
-)
+const Breadcrumbs: React.SFC<BreadcrumbsProps> = props => {
+  const items = React.Children.toArray(props.children)
+  return (
+    <Container aria-label="Breadcrumb" {...props}>
+      {items.length === 1 ? [addCaret(items[0] as React.ReactElement)] : intersperseSlashes(0)(items)}
+    </Container>
+  )
+}
 
 export default Breadcrumbs
