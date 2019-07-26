0f7dc6229d96d801119245ead90bf6ffa55fdfad
Tejas Kumar <tejas@contiamo.com>
2019-07-26 16:48:31 +0200

    **Feature:** Add "view more" to Data Table (#1161)
    * Add "view more" feature to Data Table
    
    * Explain the height - 1
    
    * Rename Kebab to something more meaningful
    
    * Move height hack

--- src/DataTable/DataTable.styled.ts
+++ src/DataTable/DataTable.styled.ts
@@ -1,6 +1,9 @@
+import { keyframes } from "@emotion/core"
+
 import styled from "../utils/styled"
 import { DataTableProps } from "./DataTable"
 import { getHeaderRowHeight } from "./DataTable.util"
+import { customScrollbar } from "../utils"
 
 export const Container = styled("div", { shouldForwardProp: prop => prop !== "width" })<{ width: string }>`
   width: ${({ width }) => width};
@@ -75,4 +78,57 @@ export const DataWrapper = styled("div")<{ numHeaders: number; rowHeight: DataTa
   position: absolute;
   width: 100%;
   top: ${({ numHeaders, rowHeight }) => numHeaders * getHeaderRowHeight(rowHeight)}px;
+`
+
+export const ViewMoreToggle = styled("div", { shouldForwardProp: prop => prop !== "height" })<{ height: number }>`
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: ${({ height }) => height}px;
+  width: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  cursor: pointer;
+  padding-right: ${({ theme }) => theme.space.small}px;
+  background: linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, rgba(255, 255, 255, 1) 46%);
+  border-radius: ${({ theme }) => theme.borderRadius}px;
+
+  > * {
+    pointer-events: none;
+  }
+`
+
+const animateIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-16px);
+  }
+  to {
+    opacity: 1;
+    transform: none;
+  }
+`
+
+export const ViewMorePopup = styled.div<{ top: number; left: number }>`
+  position: fixed;
+  padding: ${({ theme }) => theme.space.content}px;
+  font-family: ${({ theme }) => theme.font.family.code};
+  max-height: 50vh;
+  max-width: 50vw;
+  word-wrap: break-word;
+  word-break: break-all;
+  hyphens: auto;
+  overflow: auto;
+  z-index: ${({ theme }) => theme.zIndex.modal};
+  background: ${({ theme }) => theme.color.white};
+  box-shadow: ${({ theme }) => theme.shadows.popup};
+  animation: ${animateIn} 0.15s ease forwards;
+
+  ${customScrollbar}
+
+  ${({ left }) =>
+    window.innerWidth - left > 0.5 * window.innerWidth ? `left: ${left}` : `right: ${window.innerWidth - left}`}px;
+  ${({ top }) =>
+    window.innerHeight - top > 0.5 * window.innerHeight ? `top: ${top}` : `bottom: ${window.innerHeight - top}`}px;
 `
