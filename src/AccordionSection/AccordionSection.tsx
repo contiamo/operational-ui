6b1bb84daa42158fbc5ff46dd10c80e36f742573
stereobooster <stereobooster@gmail.com>
2019-08-19 16:58:19 +0200

    Accordion icon doesn't need function

--- src/AccordionSection/AccordionSection.tsx
+++ src/AccordionSection/AccordionSection.tsx
@@ -90,9 +90,9 @@ const AccordionSection: React.FC<AccordionSectionProps> = ({
         {title}
         <IconWrapper>
           {_expanded ? (
-            <ChevronUpIcon size={20} onClick={() => _toggleSection(_index)} />
+            <ChevronUpIcon size={20} onClick={() => undefined} />
           ) : (
-            <ChevronDownIcon size={20} onClick={() => _toggleSection(_index)} />
+            <ChevronDownIcon size={20} onClick={() => undefined} />
           )}
         </IconWrapper>
       </Header>
