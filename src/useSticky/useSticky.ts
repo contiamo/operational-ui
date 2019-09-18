5e8220463090b328279889bc06290522d73dac99
Tejas Kumar <tejas@contiamo.com>
2019-09-18 18:34:04 +0200

    Add useSticky hook

---
+++ src/useSticky/useSticky.ts
@@ -0,0 +1,66 @@
+import * as React from "react"
+
+/**
+ *
+ * Hook to take a component from an initial position
+ * and FIX/stick it to the screen. Typically used for
+ * nailing down positions of elements and then placing
+ * them above other elements in the viewport.
+ *
+ * Used in:
+ * - SidenavItem/Popout
+ * - ContextMenu
+ *
+ * @param inputRef - a ref to the component we would like to stick
+ * @param initialValue - initial positioning CSS values
+ */
+const useSticky = (
+  inputRef: React.Ref<HTMLDivElement>,
+  initialValue?: {
+    top: string
+    left: string
+    width?: string
+    position: "fixed" | "absolute" | "sticky" | "relative" | "static" | "initial"
+    alignment: "flex-start" | "flex-end"
+  },
+) => {
+  const defaultDisplaySettings = {
+    top: initialValue ? initialValue.top : "0",
+    left: initialValue ? initialValue.left : "100%",
+    width: initialValue ? initialValue.width : "100%",
+    position: initialValue ? initialValue.position : "absolute",
+    alignment: initialValue ? initialValue.alignment : "flex-start",
+  }
+
+  const [displaySettings, setDisplaySettings] = React.useState(defaultDisplaySettings)
+
+  React.useLayoutEffect(() => {
+    const node = inputRef && (inputRef as React.RefObject<HTMLDivElement>).current // ts can't figure out it's a refObject and thinks it's a function ref.
+
+    if (node) {
+      const draftSettings: Partial<typeof defaultDisplaySettings> = {}
+      const rect = node.getBoundingClientRect()
+      const hasEnoughRoomUnderContainer = rect.top + rect.height + window.scrollY < window.scrollY + window.innerHeight
+
+      draftSettings.left = `${rect.left}px`
+      draftSettings.width = `${rect.width}px`
+
+      if (!hasEnoughRoomUnderContainer) {
+        // open towards the top
+        ;(draftSettings.top = `${rect.top -
+          (rect.height + (node.parentElement ? node.parentElement.getBoundingClientRect().height : 0))}px`),
+          (draftSettings.alignment = "flex-end")
+      } else {
+        draftSettings.top = `${rect.top}px`
+        draftSettings.alignment = "flex-start"
+      }
+
+      draftSettings.position = "fixed"
+      setDisplaySettings({ ...displaySettings, ...draftSettings })
+    }
+  }, [inputRef])
+
+  return displaySettings
+}
+
+export default useSticky
