1def07d282834d2b53a2b1912e0c2a49de6d87cd
Tejas Kumar <tejas@contiamo.com>
2019-11-25 17:34:09 +0100

    Update testing library

--- src/useInterval/useInterval.test.tsx
+++ src/useInterval/useInterval.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { cleanup, render } from "react-testing-library"
+import { cleanup, render } from "@testing-library/react"
 
 import useInterval, { useInterval as namedHook } from "."
 
--- src/useWindowSize/useWindowSize.test.tsx
+++ src/useWindowSize/useWindowSize.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { act, cleanup, render } from "react-testing-library"
+import { act, cleanup, render } from "@testing-library/react"
 
 import { useWindowSize } from "."
 
