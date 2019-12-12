5b44d8ac9322a9d4813b92111981060063d63565
stereobooster <stereobooster@gmail.com>
2019-12-12 11:46:35 +0100

    Use reference instead of state

--- src/DataTable/useViewMore.ts
+++ src/DataTable/useViewMore.ts
@@ -5,28 +5,27 @@ import * as React from "react"
  * triggered by a click event.
  */
 const useViewMore = () => {
-  const [closeTimeoutId, setCloseTimeoutId] = React.useState<number>()
+  const closeTimeoutIdRef = React.useRef<number>()
   const [viewMorePopup, setViewMorePopup] = React.useState<{ content: string; x: number; y: number } | false>(false)
 
   const close = React.useCallback(() => {
     if (viewMorePopup) {
-      setCloseTimeoutId(window.setTimeout(() => setViewMorePopup(false), 150))
+      closeTimeoutIdRef.current = window.setTimeout(() => setViewMorePopup(false), 150)
     }
   }, [viewMorePopup])
-  React.useEffect(() => () => window.clearTimeout(closeTimeoutId), [closeTimeoutId])
+  React.useEffect(() => () => window.clearTimeout(closeTimeoutIdRef.current), [])
 
   const open = React.useCallback(
     (content: string) => (e: React.MouseEvent) => {
       e.stopPropagation()
-      window.clearTimeout(closeTimeoutId)
-      setCloseTimeoutId(undefined)
+      window.clearTimeout(closeTimeoutIdRef.current)
       setViewMorePopup({
         content,
         x: e.clientX > window.innerWidth / 2 ? e.clientX - 8 : e.clientX + 8,
         y: e.clientY > window.innerHeight / 2 ? e.clientY - 8 : e.clientY + 8,
       })
     },
-    [closeTimeoutId],
+    [],
   )
 
   const toggle = React.useCallback((content: string) => (viewMorePopup ? close() : open(content)), [viewMorePopup])
